a55eb39dcd9e9c55889b0df30859e8eecf8cd280
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-01-13 11:38:55 +0100

    Improved jest config

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,5 +1,3 @@
-require('jest-preset-angular/ngcc-jest-processor');
-
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/../../setup-jest.ts'],
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+require('jest-preset-angular/ngcc-jest-processor');
+
 module.exports = {
   projects: [
     '<rootDir>/projects/api',
