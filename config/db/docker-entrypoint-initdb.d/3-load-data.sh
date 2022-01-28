d42f42f2f0766f2f6bd86b06f19f6e8e4c869cd5
Matthijs Laan <matthijsln@gmail.com>
2022-01-28 12:18:35 +0100

    Shorten dump command

--- config/db/docker-entrypoint-initdb.d/3-load-data.sh
+++ config/db/docker-entrypoint-initdb.d/3-load-data.sh
@@ -2,8 +2,7 @@
 set -e
 
 # use the following to create the dump for example:
-# docker exec -i -u postgres -e PGPASSWORD=tailormap tailormap-viewer_db_1 pg_dump -aOx -E UTF-8 -f /tmp/3-dump.sql.script -S postgres --disable-triggers tailormap
-# docker cp tailormap-viewer_db_1:/tmp/3-dump.sql.script ./docker-entrypoint-initdb.d/3-dump.sql.script
+# docker exec -i -u postgres -e PGPASSWORD=tailormap tailormap-viewer_db_1 pg_dump -aOx -E UTF-8 -S postgres --disable-triggers tailormap > config/db/docker-entrypoint-initdb.d/3-dump.sql.script
 
 echo "Loading Tailormap default data"
 # use admin to be able to disable triggers
