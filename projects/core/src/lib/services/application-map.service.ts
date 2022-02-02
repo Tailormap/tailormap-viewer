d6153263418b3f7db4b6675ce4289ee5adac8295
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-02-02 09:46:47 +0100

    Added support for WMTS layer models without capabilities

--- projects/core/src/lib/services/application-map.service.ts
+++ projects/core/src/lib/services/application-map.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { LayerModel, LayerTypesEnum, MapService, WMSLayerModel, WMTSLayerModel } from '@tailormap-viewer/map';
+import { LayerModel, LayerTypesEnum, MapService, OgcHelper, WMSLayerModel, WMTSLayerModel } from '@tailormap-viewer/map';
 import { selectBaseLayers, selectLayers, selectMapOptions } from '../state/core.selectors';
-import { concatMap, filter, forkJoin, Observable, of, Subject, take, takeUntil } from 'rxjs';
+import { concatMap, filter, forkJoin, map, Observable, of, Subject, take, takeUntil } from 'rxjs';
 import { AppLayerModel, ServiceModel, ServiceProtocol } from '@tailormap-viewer/api';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
    providedIn: 'root',
@@ -15,6 +16,7 @@ export class ApplicationMapService implements OnDestroy {
   constructor(
     private store$: Store,
     private mapService: MapService,
+    private httpClient: HttpClient,
   ) {
     const isValidLayer = (layer: LayerModel | null): layer is LayerModel => layer !== null;
     this.store$.select(selectMapOptions)
@@ -54,12 +56,11 @@ export class ApplicationMapService implements OnDestroy {
   }
 
   private getLayersAndLayerManager$(serviceLayers: Array<{ layer: AppLayerModel; service?: ServiceModel }>) {
-    const layers = serviceLayers
-      .map(layer => {
-        return ApplicationMapService.convertAppLayerToMapLayer$(layer.layer, layer.service);
-      });
+    const layers$ = serviceLayers
+      .reverse() // until we have order add the layers in reverse order
+      .map(layer => this.convertAppLayerToMapLayer$(layer.layer, layer.service));
     return forkJoin([
-      forkJoin(layers),
+      forkJoin(layers$),
       this.mapService.getLayerManager$().pipe(take(1)),
     ]);
   }
@@ -69,22 +70,24 @@ export class ApplicationMapService implements OnDestroy {
     this.destroyed.complete();
   }
 
-  private static convertAppLayerToMapLayer$(appLayer: AppLayerModel, service?: ServiceModel): Observable<LayerModel | null> {
+  private convertAppLayerToMapLayer$(appLayer: AppLayerModel, service?: ServiceModel): Observable<LayerModel | null> {
     if (!service) {
       return of(null);
     }
     if (service.protocol === ServiceProtocol.TILED) {
-      const layer: WMTSLayerModel = {
-        id: `${appLayer.id}`,
-        layers: appLayer.displayName,
-        name: appLayer.displayName,
-        layerType: LayerTypesEnum.WMTS,
-        visible: appLayer.visible,
-        url: service.url,
-        crossOrigin: 'anonymous',
-        capabilities: service.capabilities || '',
-      };
-      return of(layer);
+      return this.getCapabilitiesForWMTS$(service)
+        .pipe(
+          map((capabilities: string): WMTSLayerModel => ({
+            id: `${appLayer.id}`,
+            layers: appLayer.displayName,
+            name: appLayer.displayName,
+            layerType: LayerTypesEnum.WMTS,
+            visible: appLayer.visible,
+            url: service.url,
+            crossOrigin: 'anonymous',
+            capabilities: capabilities || '',
+          })),
+        );
     }
     if (service.protocol === ServiceProtocol.WMS) {
       const layer: WMSLayerModel = {
@@ -99,6 +102,16 @@ export class ApplicationMapService implements OnDestroy {
       return of(layer);
     }
     return of(null);
+  }
+
+  private getCapabilitiesForWMTS$(service: ServiceModel): Observable<string> {
+    if (!!service.capabilities) {
+      return of(service.capabilities);
+    }
+    return this.httpClient.get(OgcHelper.filterOgcUrlParameters(service.url), {
+      responseType: 'text',
+      params: new HttpParams().append('REQUEST', 'GetCapabilities').append('SERVICE', 'WMTS'),
+    });
   }
 
 }
