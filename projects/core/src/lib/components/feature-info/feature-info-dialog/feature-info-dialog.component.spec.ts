b63a3ebf66faf3198e4b402ceb075217dc6d1c9f
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-02-03 13:21:15 +0100

    HTM-136: Fix refreshing feature info in dialog after state updates

--- projects/core/src/lib/components/feature-info/feature-info-dialog/feature-info-dialog.component.spec.ts
+++ projects/core/src/lib/components/feature-info/feature-info-dialog/feature-info-dialog.component.spec.ts
@@ -1,22 +1,44 @@
 import { render, screen } from '@testing-library/angular';
 import { FeatureInfoDialogComponent } from './feature-info-dialog.component';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { featureInfoStateKey, initialFeatureInfoState } from '../state/feature-info.state';
 import { SharedModule } from '@tailormap-viewer/shared';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { selectFeatureInfo, selectFeatureInfoDialogVisible } from '../state/feature-info.selectors';
 import { FeatureInfoModel } from '../models/feature-info.model';
-import { getAppLayerModel, getColumnMetadataModel, getFeatureModel, getFeaturesResponseModel } from '@tailormap-viewer/api';
+import { getAppLayerModel, getColumnMetadataModel } from '@tailormap-viewer/api';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { TestBed } from '@angular/core/testing';
 
-const getFeatureInfo = (): FeatureInfoModel[] => {
+const getFeatureInfo = (updated?: boolean): FeatureInfoModel[] => {
   return [
     {
-      features: ['1', '2', '3', '4', '5'].map(id => ({ __fid: id, attributes: { prop: 'test', prop2: 'another test', fid: id } })),
+      features: ['1', '2', '3', '4', '5']
+        .map(id => updated ? `${+(id) + 5}` : id)
+        .map(id => ({ __fid: id, attributes: { prop: 'test', prop2: 'another test', fid: id } })),
       columnMetadata: [ getColumnMetadataModel(), getColumnMetadataModel({ key: 'prop2', alias: 'Property 2' }) ],
       layer: getAppLayerModel(),
     },
   ];
+};
+
+const renderWithState = async () => {
+  await render(FeatureInfoDialogComponent, {
+    imports: [
+      SharedModule,
+      NoopAnimationsModule,
+      MatIconTestingModule,
+    ],
+    providers: [
+      provideMockStore({
+        initialState: {[featureInfoStateKey]: {...initialFeatureInfoState}},
+        selectors: [
+          { selector: selectFeatureInfo, value: getFeatureInfo() },
+          { selector: selectFeatureInfoDialogVisible, value: true },
+        ],
+      }),
+    ],
+  });
 };
 
 describe('FeatureInfoDialogComponent', () => {
@@ -36,28 +58,22 @@ describe('FeatureInfoDialogComponent', () => {
   });
 
   test('shows feature info', async () => {
-    await render(FeatureInfoDialogComponent, {
-      imports: [
-        SharedModule,
-        NoopAnimationsModule,
-        MatIconTestingModule,
-      ],
-      providers: [
-        provideMockStore({
-          initialState: {[featureInfoStateKey]: {...initialFeatureInfoState}},
-          selectors: [
-            { selector: selectFeatureInfo, value: getFeatureInfo() },
-            { selector: selectFeatureInfoDialogVisible, value: true },
-          ],
-        }),
-      ],
-    });
+    await renderWithState();
     expect((await screen.findByText(/fid/)).nextSibling?.textContent?.trim()).toEqual('1');
     expect((await screen.findByText(/Property 2/)).nextSibling?.textContent?.trim()).toEqual('another test');
     (await screen.findByText(/Next/)).click();
     expect((await screen.findByText(/fid/)).nextSibling?.textContent?.trim()).toEqual('2');
     (await screen.findByText(/Back/)).click();
     expect((await screen.findByText(/fid/)).nextSibling?.textContent?.trim()).toEqual('1');
+  });
+
+  test('updates feature info when state changes', async () => {
+    await renderWithState();
+    expect((await screen.findByText(/fid/)).nextSibling?.textContent?.trim()).toEqual('1');
+    const store = TestBed.inject(MockStore);
+    store.overrideSelector(selectFeatureInfo, getFeatureInfo(true));
+    store.refreshState();
+    expect((await screen.findByText(/fid/)).nextSibling?.textContent?.trim()).toEqual('6');
   });
 
 });
