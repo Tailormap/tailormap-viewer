c0c06e94d9adbfa50c32f57f05cfcefba89a7ffe
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-04-22 13:42:16 +0200

    HTM-326: Fixes clean up of controls after destroying components

--- projects/map/src/lib/map-service/map.service.ts
+++ projects/map/src/lib/map-service/map.service.ts
@@ -56,6 +56,7 @@ export class MapService {
           }
         }),
         map(manager => manager.addTool<T, C>(tool)),
+        tap(createdTool => toolId = createdTool?.id || ''),
       );
   }
 
