b1b85a0a73623bfecb08717ae5f1b03a17aa81c4
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-05-19 21:15:59 +0200

    Move jest transformIgnorePatterns to jest.base.config.js

--- jest.base.config.js
+++ jest.base.config.js
@@ -5,5 +5,5 @@ module.exports = {
   moduleNameMapper: {
     "^@tailormap-viewer/(.*)$": "<rootDir>/../../projects/$1/src"
   },
-  transformIgnorePatterns: ['node_modules/(?!(.*\\.mjs$|ol|observable-fns))'],
+  transformIgnorePatterns: ['node_modules/(?!(.*\\.mjs$|ol|observable-fns|quick-lru))'],
 };
