d0aa944f1e2a28858850ba485846773cebc5516a
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-06-24 16:25:38 +0200

    Fix lint

--- projects/core/src/lib/components/attribute-list/state/attribute-list.effects.ts
+++ projects/core/src/lib/components/attribute-list/state/attribute-list.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as AttributeListActions from './attribute-list.actions';
 import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
-import { filter, map, mergeMap, of } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs';
 import { AttributeListDataService } from '../services/attribute-list-data.service';
 import { Store } from '@ngrx/store';
 import { selectAttributeListDataForId } from './attribute-list.selectors';
