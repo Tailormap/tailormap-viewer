6c92df771b83aa331b706e409dff9bc457e6f788
Matthijs Laan <matthijsln@gmail.com>
2022-08-30 16:21:05 +0200

    Improve GeoServer legend quality using LEGEND_OPTIONS parameter

--- projects/core/src/lib/components/legend/legend-layer/legend-layer.component.spec.ts
+++ projects/core/src/lib/components/legend/legend-layer/legend-layer.component.spec.ts
@@ -18,4 +18,25 @@ describe('LegendLayerComponent', () => {
     expect(img.getAttribute('alt')).toEqual('Failed to load legend for Layer title');
   });
 
+  test('should render high dpi legend for GeoServer', async () => {
+    // @ts-ignore
+    jest.spyOn(global, 'window', 'get').mockImplementation(() => Object.defineProperty({}, 'devicePixelRatio', {
+      get: jest.fn().mockReturnValue(2),
+    }));
+
+    await render(LegendLayerComponent, {
+      componentProperties: {
+        url: 'http://some-url/geoserver/wms?REQUEST=GetLegendGraphic',
+        layer: getAppLayerModel({ title: 'Layer title' }),
+      },
+    });
+    const img = await screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img.getAttribute('src')).toContain('http://some-url/geoserver/wms');
+    expect(img.getAttribute('src')).toContain('LEGEND_OPTIONS=');
+    expect(img.getAttribute('src')).toContain('fontAntiAliasing%3Atrue');
+    expect(img.getAttribute('srcset')).toContain(' 2x');
+    expect(img.getAttribute('srcset')).toContain('dpi%3A180');
+  });
+
 });
--- projects/core/src/lib/components/legend/legend-layer/legend-layer.component.ts
+++ projects/core/src/lib/components/legend/legend-layer/legend-layer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 import { AppLayerModel } from '@tailormap-viewer/api';
 
 @Component({
@@ -7,7 +7,7 @@ import { AppLayerModel } from '@tailormap-viewer/api';
   styleUrls: ['./legend-layer.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LegendLayerComponent {
+export class LegendLayerComponent implements OnChanges {
 
   @Input()
   public layer: AppLayerModel | null = null;
@@ -15,6 +15,39 @@ export class LegendLayerComponent {
   @Input()
   public url: string | null = null;
 
+  public urlWithOptions: string | null = null;
+
+  public srcset = '';
+
   public failedToLoadMessage = $localize `Failed to load legend for`;
 
+  public ngOnChanges() {
+
+    this.urlWithOptions = this.url;
+    this.srcset = '';
+
+    if (this.url) {
+      try {
+        const url = new URL(this.url);
+        if (url.pathname.includes('/geoserver/') && url.searchParams.get('REQUEST') === 'GetLegendGraphic') {
+
+          const legendOptions: any = {
+            fontAntiAliasing: 'true',
+            labelMargin: '0',
+          };
+
+          const updateUrlLegendOptions = () => url.searchParams.set('LEGEND_OPTIONS', Object.entries(legendOptions).map(entry => entry.join(':')).join(';'));
+          updateUrlLegendOptions();
+          this.urlWithOptions = url.toString();
+
+          if (window.devicePixelRatio > 1) {
+            legendOptions.dpi = 180;
+            updateUrlLegendOptions();
+            this.srcset = url.toString() + ' 2x';
+          }
+        }
+      } catch(_ignored) {
+      }
+    }
+  }
 }
