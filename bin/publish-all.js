d255cd2b6ed9e9f7dfbf597d7fb37d90d7e96eed
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-10-14 13:38:18 +0200

    Publishing scripts

---
+++ bin/publish-all.js
@@ -0,0 +1,22 @@
+const {checkCleanGitRepo, runCommand} = require("./shared");
+const path = require("path");
+
+const versionArgIdx = process.argv.findIndex(a => a.indexOf('--version=') !== -1);
+const version = versionArgIdx !== -1 ? process.argv[versionArgIdx].replace('--version=', '').toLowerCase() : null;
+
+if (version === null) {
+  console.error('Supply version');
+  process.exit(1);
+}
+
+checkCleanGitRepo();
+
+const projects = ['api', 'shared', 'map', 'core'];
+(async function main() {
+  for (const project of projects) {
+    await runCommand('node', ['publish-new-release.js', '--project=' + project, '--version=' + version]);
+  }
+  await runCommand('git', ['tag', version], path.resolve(__dirname, '../'));
+})();
+
+
--- bin/publish-new-release.js
+++ bin/publish-new-release.js
@@ -3,8 +3,9 @@ const { runCommand, checkCleanGitRepo, requestProject} = require("./shared");
 
 checkCleanGitRepo();
 
-const publishRelease = (project) => {
-  runCommand('npm', ['version', 'patch'], path.resolve(__dirname, '../projects/', project))
+const publishRelease = (project, version) => {
+  const versionCommand = !!version ? ['version', version] : ['version', 'patch'];
+  runCommand('npm', versionCommand, path.resolve(__dirname, '../projects/', project))
     .then(() => runCommand('ng', ['build', project]))
     .then(() => runCommand('npm', ['publish', '--scope=@tailormap-viewer', '--registry=https://repo.b3p.nl/nexus/repository/npm-public'], path.resolve(__dirname, '../dist/', project)))
     .then(() => runCommand('git', ['add', '-A']))
@@ -14,4 +15,21 @@ const publishRelease = (project) => {
     });
 };
 
-requestProject('Select the project for which to make a release', publishRelease);
+const projectArgIdx = process.argv.findIndex(a => a.indexOf('--project=') !== -1);
+const versionArgIdx = process.argv.findIndex(a => a.indexOf('--version=') !== -1);
+
+let project;
+let version;
+
+if (projectArgIdx !== -1) {
+  project = process.argv[projectArgIdx].replace('--project=', '').toLowerCase();
+}
+if (versionArgIdx !== -1) {
+  version = process.argv[versionArgIdx].replace('--version=', '').toLowerCase();
+}
+
+if (project) {
+  publishRelease(project, version);
+} else {
+  requestProject('Select the project for which to make a release', publishRelease);
+}
