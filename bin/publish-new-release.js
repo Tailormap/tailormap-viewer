c892b62d4e832fb35ead58c2eb59167cee0b6ef4
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-10-21 13:10:45 +0200

    Updated publishing scripts

--- bin/publish-new-release.js
+++ bin/publish-new-release.js
@@ -6,56 +6,56 @@ checkCleanGitRepo();
 
 const scope = '@tailormap-viewer';
 
-const publishRelease = (project, version, dryRun) => {
-  const versionCommand = !!version ? ['version', version] : ['version', 'patch'];
-  runCommand('npm', versionCommand, path.resolve(__dirname, '../projects/', project))
-    .then(() => runCommand('ng', ['build', project]))
-    .then(() => {
-      if (dryRun) {
-        console.log('Would publish ' + project + ' to https://repo.b3p.nl/nexus/repository/npm-public, but running in dry-run mode');
-        return Promise.resolve();
+function updatePeerDependencies(project) {
+  const currentVersion = require(path.resolve(__dirname, '../projects/', project, 'package.json')).version
+  availableProjects.forEach(availableProject => {
+    const jsonPath = path.resolve(__dirname, '../projects/', availableProject, 'package.json');
+    const packageJson = require(jsonPath);
+    let madeChanges = false;
+    Object.keys(packageJson.peerDependencies).forEach(key => {
+      if (key === scope + '/' + project) {
+        packageJson.peerDependencies[key] = currentVersion;
+        madeChanges = true;
       }
-      return Promise.resolve();
-      // return runCommand('npm', ['publish', '--scope=' + scope, '--registry=https://repo.b3p.nl/nexus/repository/npm-public'], path.resolve(__dirname, '../dist/', project))
-    })
-    .then(() => {
-      const currentVersion = require(path.resolve(__dirname, '../projects/', project, 'package.json')).version
-      availableProjects.forEach(availableProject => {
-        const jsonPath = path.resolve(__dirname, '../projects/', availableProject, 'package.json');
-        const packageJson = require(jsonPath);
-        let madeChanges = false;
-        Object.keys(packageJson.peerDependencies).forEach(key => {
-          if (key === scope + '/' + project) {
-            packageJson.peerDependencies[key] = currentVersion;
-            madeChanges = true;
-          }
-        });
-        fs.writeFileSync(jsonPath, JSON.stringify(packageJson, null, 2));
-      });
-      return Promise.resolve();
-    })
-    .then(() => runCommand('git', ['add', '-A']))
-    .then(() => {
-      const currentVersion = require(path.resolve(__dirname, '../projects/', project, 'package.json')).version
-      const message = `Released version ${currentVersion} of ${project} project`;
-      if (dryRun) {
-        console.log('Would commit: ' + message + ', but running in dry-run mode');
-        return Promise.resolve();
-      }
-      return Promise.resolve();
-      // return runCommand('git', ['commit', '-m', `Released version ${currentVersion} of ${project} project`]);
-    })
-    .catch(error => {
-      console.log('Error occurred: ', error);
     });
-};
+    fs.writeFileSync(jsonPath, JSON.stringify(packageJson, null, 2));
+  });
+}
+
+async function publishRelease(project, version, dryRun) {
+  const versionCommand = !!version ? ['version', version] : ['version', 'patch'];
+  await runCommand('npm', versionCommand, path.resolve(__dirname, '../projects/', project));
+  await runCommand('ng', ['build', project]);
+  if (dryRun) {
+    console.log('Would publish ' + project + ' to https://repo.b3p.nl/nexus/repository/npm-public, but running in dry-run mode');
+  } else {
+    // await runCommand('npm', ['publish', '--scope=' + scope, '--registry=https://repo.b3p.nl/nexus/repository/npm-public'], path.resolve(__dirname, '../dist/', project));
+  }
+  updatePeerDependencies(project);
+  if (dryRun) {
+    console.log('Would add all changed files to Git, but running in dry-run mode');
+  } else {
+    await runCommand('git', ['add', '-A']);
+  }
+  const currentVersion = require(path.resolve(__dirname, '../projects/', project, 'package.json')).version
+  const message = `Released version ${currentVersion} of ${project} project`;
+  if (dryRun) {
+    console.log('Would commit: ' + message + ', but running in dry-run mode');
+  } else {
+    // await runCommand('git', ['commit', '-m', `Released version ${currentVersion} of ${project} project`])
+  }
+}
 
 const project = getCliArgument('--project');
 const version = getCliArgument('--version');
 const dryRun = hasCliArgument('--dry-run');
 
 if (project) {
-  publishRelease(project, version, dryRun);
+  try {
+    publishRelease(project, version, dryRun);
+  } catch (e) {
+    console.log('Error occured', e);
+  }
 } else {
   requestProject('Select the project for which to make a release', publishRelease);
 }
