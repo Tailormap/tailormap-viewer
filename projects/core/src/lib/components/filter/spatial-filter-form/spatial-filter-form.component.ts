1ebf88bcef0d9e4a0a398370299511b9680b7fa2
Geert Plaisier <geert.plaisier@gnamic.nl>
2022-11-30 17:54:32 +0100

    HTM-553: Add buffering option for spatial filter

--- projects/core/src/lib/components/filter/spatial-filter-form/spatial-filter-form.component.ts
+++ projects/core/src/lib/components/filter/spatial-filter-form/spatial-filter-form.component.ts
@@ -5,7 +5,7 @@ import { ExtendedAppLayerModel } from '../../../map/models';
 import { BehaviorSubject, map, Observable, of, Subject, take, takeUntil } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { DrawingFeatureTypeEnum } from '../../../map/models/drawing-feature-type.enum';
-import { DrawingToolEvent, MapService } from '@tailormap-viewer/map';
+import { DrawingToolEvent, MapService, MapStyleModel } from '@tailormap-viewer/map';
 import { nanoid } from 'nanoid';
 import { SpatialFilterGeometry, SpatialFilterModel } from '../../../filter/models/spatial-filter.model';
 import { FeatureStylingHelper } from '../../../shared/helpers/feature-styling.helper';
@@ -17,6 +17,7 @@ import { closeForm, setSelectedFilterGroup } from '../state/filter-component.act
 import { CreateFilterService } from '../services/create-filter.service';
 import { FeatureModel, FeatureModelAttributes } from '@tailormap-viewer/api';
 import { RemoveFilterService } from '../services/remove-filter.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'tm-spatial-filter-form',
@@ -26,16 +27,22 @@ import { RemoveFilterService } from '../services/remove-filter.service';
 })
 export class SpatialFilterFormComponent implements OnInit, OnDestroy {
 
-  private DEFAULT_STYLE = FeatureStylingHelper.getDefaultHighlightStyle('filter-drawing-style', {
+  private DRAWING_STYLE: Partial<MapStyleModel> = {
     pointType: undefined,
     fillColor: '#6236ff',
-    fillOpacity: 10,
+    fillOpacity: 30,
     strokeWidth: 2,
+  };
+
+  private DEFAULT_STYLE = (feature: FeatureModel) => FeatureStylingHelper.getDefaultHighlightStyle('filter-drawing-style', {
+    ...this.DRAWING_STYLE,
+    buffer: feature.attributes?.buffer,
   });
 
-  private SELECTED_STYLE = () => FeatureStylingHelper.getDefaultHighlightStyle('filter-selected-style', {
-    ...this.DEFAULT_STYLE,
+  private SELECTED_STYLE = (feature: FeatureModel) => FeatureStylingHelper.getDefaultHighlightStyle('filter-selected-style', {
+    ...this.DRAWING_STYLE,
     isSelected: true,
+    buffer: feature.attributes?.buffer,
   });
 
   private store$ = inject(Store);
@@ -58,16 +65,24 @@ export class SpatialFilterFormComponent implements OnInit, OnDestroy {
   public selectedLayers = new FormControl<number[]>([], {
     nonNullable: true,
   });
-  private geometriesSubject$ = new BehaviorSubject<SpatialFilterGeometry[]>([]);
+
+  public buffer = new FormControl<number>(0, {
+    nonNullable: true,
+  });
+
+  private geometriesSubject$ = new BehaviorSubject<Array<SpatialFilterGeometry & {buffer?: number}>>([]);
   private geometries$ = this.geometriesSubject$.asObservable();
   private drawingFeatures$: Observable<FeatureModel[]> = this.geometries$
     .pipe(map(geometries => {
       return geometries.map<FeatureModel>(geom => ({
         __fid: geom.id,
         geometry: geom.geometry,
-        attributes: {},
+        attributes: {
+          buffer: geom.buffer,
+        },
       }));
     }));
+  public hasGeometry$ = this.geometries$.pipe(map(geometries => geometries.length > 0));
   public currentGroup: FilterGroupModel<SpatialFilterModel> | undefined;
 
   public ngOnInit(): void {
@@ -85,9 +100,13 @@ export class SpatialFilterFormComponent implements OnInit, OnDestroy {
       this.DEFAULT_STYLE,
     ).pipe(takeUntil(this.destroyed)).subscribe();
 
+    this.buffer.valueChanges
+      .pipe(takeUntil(this.destroyed), debounceTime(250))
+      .subscribe(() => this.updateCreateGroup());
+
     this.selectedLayers.valueChanges
       .pipe(takeUntil(this.destroyed))
-      .subscribe((layers) => this.updateCreateGroup(layers, this.geometriesSubject$.value));
+      .subscribe(() => this.updateCreateGroup());
   }
 
   public ngOnDestroy(): void {
@@ -99,14 +118,14 @@ export class SpatialFilterFormComponent implements OnInit, OnDestroy {
     const currentGeometries = this.geometriesSubject$.value;
     const updatedGeometries = [ ...currentGeometries, { id: nanoid(), geometry: $event.geometry }];
     this.geometriesSubject$.next(updatedGeometries);
-    this.updateCreateGroup(this.selectedLayers.value, updatedGeometries);
+    this.updateCreateGroup(updatedGeometries);
   }
 
   public featureRemoved($event: string) {
     const currentGeometries = this.geometriesSubject$.value;
     const updatedGeometries = currentGeometries.filter(geom => geom.id !== $event);
     this.geometriesSubject$.next(updatedGeometries);
-    this.updateCreateGroup(this.selectedLayers.value, updatedGeometries);
+    this.updateCreateGroup(updatedGeometries);
   }
 
   public save() {
@@ -129,29 +148,43 @@ export class SpatialFilterFormComponent implements OnInit, OnDestroy {
       });
   }
 
+  private getBufferValue(): number | undefined {
+    const buffer = this.buffer.value;
+    if (!buffer || buffer === 0 || isNaN(+(buffer))) {
+      return undefined;
+    }
+    return +(buffer);
+  }
+
   private setForm(group?: FilterGroupModel) {
     if (!FilterTypeHelper.isSpatialFilterGroup(group)) {
       this.currentGroup = undefined;
       this.selectedLayers.patchValue([], { emitEvent: false });
       this.geometriesSubject$.next([]);
+      this.buffer.patchValue(0, { emitEvent: false });
       this.cdr.detectChanges();
       return;
     }
+    const buffer = group.filters.length > 0 ? group.filters[0].buffer : undefined;
     const geometries = group.filters.reduce<SpatialFilterGeometry[]>((g, filter) => {
-      return [ ...g, ...filter.geometries ];
+      return [ ...g, ...filter.geometries.map(geom => ({ ...geom, buffer: filter.buffer })) ];
     }, []);
     this.currentGroup = group;
     this.geometriesSubject$.next(geometries);
+    this.buffer.patchValue(buffer || 0, { emitEvent: false });
     this.selectedLayers.patchValue(group.layerIds, { emitEvent: false });
     this.cdr.detectChanges();
   }
 
-  private updateCreateGroup(layers: number[], geometries: SpatialFilterGeometry[]) {
+  private updateCreateGroup(updatedGeometries?: SpatialFilterGeometry[]) {
+    const geometries = updatedGeometries || this.geometriesSubject$.value;
+    const layers = this.selectedLayers.value;
     if (layers.length === 0 || geometries.length === 0) {
       return;
     }
+    const buffer = this.getBufferValue();
     if (!this.currentGroup) {
-      this.createFilterService.createSpatialFilterGroup$(geometries || [], layers)
+      this.createFilterService.createSpatialFilterGroup$(geometries || [], layers, buffer)
         .pipe(take(1))
         .subscribe(group => {
           this.store$.dispatch(addFilterGroup({ group }));
@@ -159,7 +192,7 @@ export class SpatialFilterFormComponent implements OnInit, OnDestroy {
         });
       return;
     }
-    this.createFilterService.updateSpatialFilterGroup$(this.currentGroup, geometries, layers)
+    this.createFilterService.updateSpatialFilterGroup$(this.currentGroup, geometries, layers, buffer)
       .pipe(take(1))
       .subscribe(updatedFilterGroup => {
         this.store$.dispatch(updateFilterGroup({ filterGroup: updatedFilterGroup }));
--- projects/map/src/typings.d.ts
+++ projects/map/src/typings.d.ts
@@ -1,1 +1,23 @@
 declare function $localize(messageParts: TemplateStringsArray, ...expressions: readonly any[]): string;
+
+declare module 'jsts/org/locationtech/jts/io' {
+  export class OL3Parser {
+    constructor();
+    public inject(...geometryFactory: any): void;
+    public read(geometry: any): any;
+    public write(geometry: any): any;
+  }
+}
+
+declare module 'jsts/org/locationtech/jts/operation/buffer' {
+  export class BufferOp {
+    public static bufferOp(geom: any, distance: number): any;
+  }
+}
+
+declare module 'jsts/org/locationtech/jts/geom' {
+  export class Geometry {
+    constructor(geom?: any): any;
+    public difference(geom: any): any;
+  }
+}
