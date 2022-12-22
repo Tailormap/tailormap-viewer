ec6720c08e5f83c2c682d0af1b188e0e7d3d02d8
Puck Meerburg <puck@puckipedia.com>
2022-12-22 13:00:46 +0000

    [HTM-546] Add geolocation button

---
+++ projects/core/src/lib/components/toolbar/geolocation/geolocation.component.ts
@@ -0,0 +1,112 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { MapService, CoordinateHelper } from '@tailormap-viewer/map';
+import { Subject, takeUntil, take } from 'rxjs';
+import { FeatureModel } from '@tailormap-viewer/api';
+import { ApplicationStyleService } from '../../../services/application-style.service';
+
+@Component({
+  selector: 'tm-geolocation',
+  templateUrl: './geolocation.component.html',
+  styleUrls: ['./geolocation.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GeolocationComponent implements OnInit, OnDestroy {
+
+  private destroyed = new Subject();
+  private featureGeom = new Subject<FeatureModel[]>();
+
+  private activeWatch: number | undefined = undefined;
+
+  public hasGeolocation = navigator.geolocation !== undefined;
+  public isWatching = false;
+  public hasFix = false;
+
+  constructor(
+    private mapService: MapService,
+    private cdr: ChangeDetectorRef,
+  ) { }
+
+  public ngOnInit(): void {
+    this.mapService.renderFeatures$('geolocation-layer', this.featureGeom.asObservable(), f => {
+      const primaryColor = ApplicationStyleService.getPrimaryColor();
+
+      if (f.__fid === 'geolocation-point') {
+        return {
+          styleKey: 'geolocation-point-style',
+          zIndex: 1000,
+          pointType: 'circle',
+          pointSize: 7,
+          pointFillColor: primaryColor,
+          pointStrokeColor: '#ffffff',
+          pointStrokeWidth: 2,
+        };
+      } else {
+        return {
+          styleKey: 'geolocation-shadow-style',
+          zIndex: 999,
+          fillColor: primaryColor,
+          fillOpacity: 20,
+          strokeColor: primaryColor,
+          strokeOpacity: 40,
+        };
+      }
+     }).pipe(takeUntil(this.destroyed)).subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyed.next(null);
+    this.destroyed.complete();
+  }
+
+  private positionSuccess(pos: GeolocationPosition) {
+    this.mapService.getProjectionCode$()
+      .pipe(take(1))
+      .subscribe(projectionCode => {
+          const point = CoordinateHelper.projectCoordinates([ pos.coords.longitude, pos.coords.latitude ], 'EPSG:4326', projectionCode);
+          if (point === undefined) {
+              return;
+          }
+
+          const pointWkt = `POINT(${point[0]} ${point[1]})`;
+          const circleWkt = CoordinateHelper.circleFromWGS84CoordinatesAndRadius([ pos.coords.longitude, pos.coords.latitude ], pos.coords.accuracy, projectionCode);
+
+          this.featureGeom.next([
+            { __fid: 'geolocation-point', geometry: pointWkt, attributes: {} },
+            { __fid: 'geolocation-shadow', geometry: circleWkt, attributes: {} },
+          ]);
+
+          if (!this.hasFix) {
+            this.hasFix = true;
+            this.mapService.zoomTo(circleWkt, projectionCode);
+            this.cdr.detectChanges();
+          }
+      });
+  }
+
+  private cancelGeolocation() {
+      if (this.activeWatch !== undefined) {
+        navigator.geolocation.clearWatch(this.activeWatch);
+        this.activeWatch = undefined;
+      }
+
+      this.featureGeom.next([]);
+      this.isWatching = false;
+      this.hasFix = false;
+      this.cdr.detectChanges();
+  }
+
+  private positionError(_e: GeolocationPositionError) {
+      this.cancelGeolocation();
+  }
+
+  public onClick(): void {
+    if (this.activeWatch === undefined) {
+      this.activeWatch = navigator.geolocation.watchPosition(this.positionSuccess.bind(this), this.positionError.bind(this), { enableHighAccuracy: true });
+      this.isWatching = true;
+      this.cdr.detectChanges();
+    } else {
+      this.cancelGeolocation();
+    }
+  }
+
+}
