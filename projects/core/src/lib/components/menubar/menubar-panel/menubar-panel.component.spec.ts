15ff4b2b299246ce4bcba2f4fbc000f87331ed4c
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-02-01 12:34:26 +0100

    Fixes tests

--- projects/core/src/lib/components/menubar/menubar-panel/menubar-panel.component.spec.ts
+++ projects/core/src/lib/components/menubar/menubar-panel/menubar-panel.component.spec.ts
@@ -2,37 +2,43 @@ import { render, screen } from '@testing-library/angular';
 import { MenubarPanelComponent } from './menubar-panel.component';
 import { SharedModule } from '@tailormap-viewer/shared';
 import { MenubarService } from '../menubar.service';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import userEvent from '@testing-library/user-event';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
+
+const getMenuBarServiceMock = (initialValue: { componentId: string; dialogTitle: string } | null = null) => {
+  const activeComponent$ = new BehaviorSubject(initialValue);
+  return {
+    provide: MenubarService,
+    useValue: {
+      activeComponent$,
+      getActiveComponent$: () => activeComponent$.asObservable(),
+      closePanel: jest.fn().mockImplementation(() => activeComponent$.next(null)),
+    },
+  };
+};
 
 describe('MenubarPanelComponent', () => {
 
   test('does not render panel contents if active component is false', async () => {
     await render(MenubarPanelComponent, {
       imports: [SharedModule],
-      providers: [
-        { provide: MenubarService, useValue: { getActiveComponent$: () => of(null) } },
-      ],
+      providers: [getMenuBarServiceMock()],
     });
     expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 
   test('renders active component', async () => {
-    const menubarServiceMock = {
-      getActiveComponent$: () => of({ componentId: 'TOC', dialogTitle: 'Available layers' }),
-      closePanel: jest.fn(),
-    };
+    const menubarServiceMock = getMenuBarServiceMock({ componentId: 'TOC', dialogTitle: 'Available layers' });
+    const closePanelFn = menubarServiceMock.useValue.closePanel;
     await render(MenubarPanelComponent, {
       imports: [ SharedModule, MatIconTestingModule ],
-      providers: [
-        { provide: MenubarService, useValue: menubarServiceMock },
-      ],
+      providers: [menubarServiceMock],
     });
     expect(screen.getByText('Available layers')).toBeInTheDocument();
     expect(screen.queryByRole('button')).toBeInTheDocument();
     await userEvent.click(screen.getByRole('button'));
-    expect(menubarServiceMock.closePanel).toHaveBeenCalled();
+    expect(closePanelFn).toHaveBeenCalled();
   });
 
 });
