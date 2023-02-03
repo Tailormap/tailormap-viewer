c1ff1e0600f648c87bcac9ae04ffef5e26b856e9
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-02-03 11:01:08 +0100

    Fix admin build

--- projects/admin-core/src/lib/admin-core-routing.module.ts
+++ projects/admin-core/src/lib/admin-core-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminHomePageComponent } from './pages/admin-home-page/admin-home-page.component';
 import { GeoRegistryPageComponent } from './pages/geo-registry-page/geo-registry-page.component';
 import { RoutesEnum } from './routes';
-import { GeoServiceSourcesPageComponent } from './pages/geo-service-sources-page/geo-service-sources-page.component';
-import { GeoRegistryAttributesPageComponent } from './pages/geo-service-attributes-page/geo-registry-attributes-page.component';
+import { GeoRegistrySourcesPageComponent } from './pages/geo-registry-sources-page/geo-registry-sources-page.component';
+import { GeoRegistryAttributesPageComponent } from './pages/geo-registry-attributes-page/geo-registry-attributes-page.component';
 
 const routes: Routes = [
   { path: RoutesEnum.GEO_REGISTRY, component: GeoRegistryPageComponent },
-  { path: RoutesEnum.GEO_REGISTRY_SOURCES, component: GeoServiceSourcesPageComponent },
+  { path: RoutesEnum.GEO_REGISTRY_SOURCES, component: GeoRegistrySourcesPageComponent },
   { path: RoutesEnum.GEO_REGISTRY_ATTRIBUTES, component: GeoRegistryAttributesPageComponent },
   { path: RoutesEnum.ADMIN_HOME, component: AdminHomePageComponent },
 ];
