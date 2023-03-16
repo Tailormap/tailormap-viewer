7225bc0bec7861f6236bb5502e2b7b8a4ad48596
Matthijs Laan <matthijsln@gmail.com>
2023-03-16 17:45:47 +0100

    Require user-entered id when creating service

--- projects/admin-core/src/lib/catalog/geo-service-form/geo-service-form.component.spec.ts
+++ projects/admin-core/src/lib/catalog/geo-service-form/geo-service-form.component.spec.ts
@@ -15,16 +15,17 @@ describe('GeoServiceFormComponent', () => {
         } as any,
       },
     });
+    await userEvent.type(await screen.findByPlaceholderText('ID'), 'test');
     await userEvent.type(await screen.findByPlaceholderText('URL'), 'http://localhost.test');
     await waitFor(() => {
       expect(changedFn).toHaveBeenCalledTimes(1);
-      expect(changedFn).toHaveBeenCalledWith({ title: '', url: 'http://localhost.test', protocol: 'wms' });
+      expect(changedFn).toHaveBeenCalledWith({ id: 'test', title: '', url: 'http://localhost.test', protocol: 'wms' });
     });
     await userEvent.click(await screen.findByText('wms'));
     await userEvent.click(await screen.findByText('wmts'));
     await waitFor(() => {
       expect(changedFn).toHaveBeenCalledTimes(2);
-      expect(changedFn).toHaveBeenNthCalledWith(2, { title: '', url: 'http://localhost.test', protocol: 'wmts' });
+      expect(changedFn).toHaveBeenNthCalledWith(2, { id: 'test', title: '', url: 'http://localhost.test', protocol: 'wmts' });
     });
   });
 
