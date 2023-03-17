b37909d28b8a71c0b64d0aac4de7c632ba88010e
Mark Prins <1165786+mprins@users.noreply.github.com>
2023-03-17 11:19:51 +0100

    [HTM-538] F - Location search in ESPG:3857 viewer zooms to EPSG:4326 coordinates (#328)
    * [HTM-538] Add a utility to transform a WKT geometry string to a different projection
    
    * Reproject the searchresult to match the map projection

--- projects/map/src/lib/helpers/feature.helper.spec.ts
+++ projects/map/src/lib/helpers/feature.helper.spec.ts
@@ -2,6 +2,7 @@ import { Circle, Point } from 'ol/geom';
 import { FeatureHelper } from './feature.helper';
 import { MapUnitEnum } from '../models/map-unit.enum';
 import { Projection } from 'ol/proj';
+import { ProjectionsHelper } from './projections.helper';
 
 const projection = {
   getUnits: () => MapUnitEnum.m,
@@ -31,5 +32,35 @@ describe('FeatureTypesHelper', () => {
 
   test('checks getWKT() for non-standard but not linearized circle with maximum number of decimals for meter units of measure', () => {
     expect(FeatureHelper.getWKT(new Circle([ 1.3496582958, 3.12345 ], 3.12345), projection, false)).toBe('CIRCLE(1.35 3.12 3.12)');
+  });
+
+  test('checks transformGeometry point from EPSG:4326 to EPSG:3857', () => {
+    expect(FeatureHelper.transformGeometry('POINT(1 2)', 'EPSG:4326', 'EPSG:3857')).toContain('POINT(111319.4907');
+    expect(FeatureHelper.transformGeometry('POINT(1 2)', 'EPSG:4326', 'EPSG:3857')).toContain(' 222684.2085');
+  });
+
+  test('checks transformGeometry polygon from EPSG:4326 to EPSG:3857', () => {
+    expect(FeatureHelper.transformGeometry('POLYGON ((1 2, 1 3, 2 3, 1 3, 1 2))', 'EPSG:4326', 'EPSG:3857')).toContain('POLYGON((111319.4907');
+    expect(FeatureHelper.transformGeometry('POLYGON ((1 2, 1 3, 2 3, 1 3, 1 2))', 'EPSG:4326', 'EPSG:3857')).toContain(' 222684.2085');
+  });
+
+  test('checks transformGeometry polygon to same crs', () => {
+    expect(FeatureHelper.transformGeometry('POLYGON ((1 2, 1 3, 2 3, 1 3, 1 2))', 'EPSG:3857', 'EPSG:3857')).toBe('POLYGON ((1 2, 1 3, 2 3, 1 3, 1 2))');
+  });
+
+  test('checks transformGeometry polygon from EPSG:28992 to EPSG:3857', () => {
+    ProjectionsHelper.initProjection('EPSG:28992',
+      // eslint-disable-next-line max-len
+      '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs');
+    // eslint-disable-next-line max-len
+    const wktGeom = 'POLYGON ((131104 460577, 129823 461804, 129495 462130, 128732 461629, 128944 461027, 129420 460143, 129722 459834, 129823 459563, 130115 459450, 130698 458799, 130791 458756, 131164 459552, 131574 460125, 131104 460577))';
+    // transformed to EPSG:3857 using postgis
+    // POLYGON((560849.2581363895 6824187.345771471,558756.4678094786 6826177.236152881,558220.3448005843 6826706.020960536,
+    //    556983.7628036102 6825882.722527874,557333.6419826547 6824903.165435475,558114.9992614571 6823466.3175152205,
+    //    558608.5089436477 6822965.239498022,558774.9233316716 6822524.435377617,559250.4864285775 6822342.685742314,
+    //    560203.2990491522 6821286.592399324,560354.787083448 6821217.268622127,560954.7970522561 6822517.280664146,
+    //    561616.8607501892 6823454.284349732,560849.2581363895 6824187.345771471))
+    expect(FeatureHelper.transformGeometry(wktGeom, 'EPSG:28992', 'EPSG:3857')).toContain('POLYGON((560849.2581');
+    expect(FeatureHelper.transformGeometry(wktGeom, 'EPSG:28992', 'EPSG:3857')).toContain(' 6824187.345');
   });
 });
