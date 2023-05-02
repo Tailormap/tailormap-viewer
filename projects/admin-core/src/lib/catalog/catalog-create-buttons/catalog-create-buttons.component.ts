33d9468b5763c01207c9a43b21b769405a5e0f10
Matthijs Laan <matthijsln@gmail.com>
2023-05-02 15:25:28 +0200

    Expand (parent) node when adding catalog items

--- projects/admin-core/src/lib/catalog/catalog-create-buttons/catalog-create-buttons.component.ts
+++ projects/admin-core/src/lib/catalog/catalog-create-buttons/catalog-create-buttons.component.ts
@@ -55,6 +55,7 @@ export class CatalogCreateButtonsComponent implements OnInit, OnDestroy {
     }).afterClosed().pipe(takeUntil(this.destroyed)).subscribe(node => {
       if (node) {
         this.router.navigateByUrl(CatalogRouteHelper.getCatalogNodeUrl({ id: node.id }));
+        this.store$.dispatch(expandTree({ id: node.id, nodeType: CatalogTreeModelTypeEnum.CATALOG_NODE_TYPE }));
       }
     });
   }
@@ -70,6 +71,7 @@ export class CatalogCreateButtonsComponent implements OnInit, OnDestroy {
     }).afterClosed().pipe(takeUntil(this.destroyed)).subscribe(createdService => {
       if (createdService) {
         this.router.navigateByUrl(CatalogRouteHelper.getGeoServiceUrl({ id: createdService.id, catalogNodeId: parentNode }));
+        this.store$.dispatch(expandTree({ id: parentNode, nodeType: CatalogTreeModelTypeEnum.CATALOG_NODE_TYPE }));
         this.store$.dispatch(expandTree({ id: createdService.id, nodeType: CatalogTreeModelTypeEnum.SERVICE_TYPE }));
       }
     });
@@ -86,6 +88,7 @@ export class CatalogCreateButtonsComponent implements OnInit, OnDestroy {
     }).afterClosed().pipe(takeUntil(this.destroyed)).subscribe(featureSource => {
       if (featureSource) {
         this.router.navigateByUrl(CatalogRouteHelper.getFeatureSourceUrl({ id: featureSource.id, catalogNodeId: parentNode }));
+        this.store$.dispatch(expandTree({ id: parentNode, nodeType: CatalogTreeModelTypeEnum.CATALOG_NODE_TYPE }));
         this.store$.dispatch(expandTree({ id: featureSource.id, nodeType: CatalogTreeModelTypeEnum.FEATURE_SOURCE_TYPE }));
       }
     });
