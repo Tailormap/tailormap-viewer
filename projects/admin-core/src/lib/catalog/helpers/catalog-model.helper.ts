38a6269f04a2216611bda31a6fe7723e06cede84
Matthijs Laan <matthijsln@gmail.com>
2023-05-10 13:32:54 +0200

    Make layer id globally unique with service id prefix

--- projects/admin-core/src/lib/catalog/helpers/catalog-model.helper.ts
+++ projects/admin-core/src/lib/catalog/helpers/catalog-model.helper.ts
@@ -7,14 +7,12 @@ import { ExtendedFeatureTypeModel } from '../models/extended-feature-type.model'
 export class CatalogModelHelper {
 
   public static getExtendedGeoServiceLayer(geoService: GeoServiceWithLayersModel, catalogNodeId: string): ExtendedGeoServiceLayerModel[] {
-    const serviceLayers = geoService.layers.map((layer, idx) => {
-      return {
-        ...layer,
-        id: `${geoService.id}_${layer.id}`,
-        serviceId: `${geoService.id}`,
-        catalogNodeId,
-      };
-    });
+    const serviceLayers = geoService.layers.map(layer => ({
+      ...layer,
+      id: `${geoService.id}_${layer.id}`,
+      serviceId: `${geoService.id}`,
+      catalogNodeId,
+    }));
     return serviceLayers.map<ExtendedGeoServiceLayerModel>(layer => ({
       ...layer,
       children: layer.children // map children to point to ID instead of name
