8a5d7917a664969889cfd1f438a6e407196769bd
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-08-24 13:49:00 +0200

    HTM-847: Allow to set an alias for each attribute

--- projects/admin-core/src/lib/catalog/feature-type-attributes/feature-type-attributes.component.ts
+++ projects/admin-core/src/lib/catalog/feature-type-attributes/feature-type-attributes.component.ts
@@ -1,8 +1,13 @@
-import { Component, ChangeDetectionStrategy, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import {
+  Component, ChangeDetectionStrategy, Input, OnChanges, Output, EventEmitter, SimpleChanges, DestroyRef, OnInit,
+} from '@angular/core';
 import { AttributeDescriptorModel, FeatureTypeSettingsModel } from '@tailormap-admin/admin-api';
 import { FeatureTypeAttributeHelper } from '../helpers/feature-type-attribute.helper';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { ArrayHelper } from '@tailormap-viewer/shared';
+import { FormControl, FormGroup } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tm-admin-feature-type-attributes',
@@ -12,7 +17,7 @@ import { ArrayHelper } from '@tailormap-viewer/shared';
 })
 export class FeatureTypeAttributesComponent implements OnChanges {
 
-  public readonly columns = [ 'enabled', 'name', 'type', 'sort' ];
+  public readonly columns = [ 'enabled', 'name', 'type', 'alias', 'sort' ];
 
   @Input()
   public attributes: AttributeDescriptorModel[] = [];
@@ -26,20 +31,46 @@ export class FeatureTypeAttributesComponent implements OnChanges {
   @Output()
   public attributeOrderChanged = new EventEmitter<string[]>();
 
+  @Output()
+  public aliasesChanged = new EventEmitter<Array<{ attribute: string; alias: string | undefined }>>();
+
+  public aliasForm: FormGroup = new FormGroup({});
+
   public enabledAttributes = new Set<string>();
   public allAttributesEnabled = false;
   public someAttributesDisabled = false;
   public dataAttributes: AttributeDescriptorModel[] = [];
   public geomAttributes: AttributeDescriptorModel[] = [];
 
-  constructor() { }
+  constructor(
+    private destroyRef: DestroyRef,
+  ) {
+    this.aliasForm.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(values => {
+        const updatedAliases = Object.keys(values)
+          .map(attribute => ({ attribute, alias: values[attribute] || undefined }));
+        this.aliasesChanged.emit(updatedAliases);
+      });
+  }
 
-  public ngOnChanges(): void {
-    if (this.attributes) {
+  public ngOnChanges(changes: SimpleChanges): void {
+    const attributesChanged = !!changes['attributes'];
+    if (this.attributes && (attributesChanged || this.changedSettings(changes, 'attributeOrder'))) {
       const attributeOrder = this.featureTypeSettings?.attributeOrder || [];
       const sortedAttributes = [...this.attributes].sort(ArrayHelper.getArraySorter('name', attributeOrder));
       this.dataAttributes = sortedAttributes.filter(a => !FeatureTypeAttributeHelper.isGeometryType(a.type));
       this.geomAttributes = sortedAttributes.filter(a => FeatureTypeAttributeHelper.isGeometryType(a.type));
+    }
+    if (this.attributes && attributesChanged) {
+      const aliases = this.featureTypeSettings?.attributeSettings || {};
+      Object.keys(this.aliasForm.controls).forEach(ctrl => this.aliasForm.removeControl(ctrl));
+      this.dataAttributes.forEach(att => {
+        const control = new FormControl<string>(aliases[att.name]?.title || '');
+        this.aliasForm.addControl(att.name, control, { emitEvent: false });
+      });
+    }
+    if (this.changedSettings(changes, 'hideAttributes')) {
       const hideAttributes = new Set(this.featureTypeSettings?.hideAttributes || []);
       this.enabledAttributes = new Set(this.dataAttributes
         .map(a => a.name)
@@ -65,6 +96,13 @@ export class FeatureTypeAttributesComponent implements OnChanges {
     const attributes = [...this.dataAttributes];
     moveItemInArray(attributes, $event.previousIndex, $event.currentIndex);
     this.attributeOrderChanged.emit(attributes.map(a => a.name));
+  }
+
+  private changedSettings(changes: SimpleChanges, item: keyof FeatureTypeSettingsModel) {
+    if (!changes['featureTypeSettings'] || !changes['featureTypeSettings'].previousValue) {
+      return true;
+    }
+    return this.featureTypeSettings && this.featureTypeSettings[item] !== changes['featureTypeSettings'].previousValue[item];
   }
 
 }
--- projects/admin-core/src/lib/catalog/feature-type-details/feature-type-details.component.ts
+++ projects/admin-core/src/lib/catalog/feature-type-details/feature-type-details.component.ts
@@ -6,7 +6,8 @@ import { Store } from '@ngrx/store';
 import { FeatureSourceService } from '../services/feature-source.service';
 import { ExtendedFeatureTypeModel } from '../models/extended-feature-type.model';
 import { FeatureTypeUpdateModel } from '../models/feature-source-update.model';
-import { FeatureTypeSettingsModel } from '@tailormap-admin/admin-api';
+import { AttributeSettingsModel, FeatureTypeModel, FeatureTypeSettingsModel } from '@tailormap-admin/admin-api';
+import { ObjectHelper } from '@tailormap-viewer/shared';
 
 @Component({
   selector: 'tm-admin-feature-type-details',
@@ -60,13 +61,21 @@ export class FeatureTypeDetailsComponent implements OnInit, OnDestroy {
     this.destroyed.complete();
   }
 
-  public save(featureTypeId: string) {
+  public save(featureType: FeatureTypeModel) {
     if (!this.updatedFeatureTypeSubject.value) {
       return;
     }
-    const updatedFeatureType = { ...this.updatedFeatureTypeSubject.value };
+    const currentUpdatedValue = this.updatedFeatureTypeSubject.value;
+    const updatedFeatureType: FeatureTypeUpdateModel = {
+      ...currentUpdatedValue,
+      settings: {
+        attributeSettings: currentUpdatedValue?.settings?.attributeSettings || featureType.settings.attributeSettings || {},
+        hideAttributes: currentUpdatedValue?.settings?.hideAttributes || featureType.settings.hideAttributes || [],
+        attributeOrder: currentUpdatedValue?.settings?.attributeOrder || featureType.settings.attributeOrder || [],
+      },
+    };
     this.savingSubject.next(true);
-    this.featureSourceService.updateFeatureType$(featureTypeId, updatedFeatureType)
+    this.featureSourceService.updateFeatureType$(featureType.id, updatedFeatureType)
       .subscribe(result => {
         this.savingSubject.next(false);
         if (result) {
@@ -88,25 +97,33 @@ export class FeatureTypeDetailsComponent implements OnInit, OnDestroy {
         hideAttributes.add(change.attribute);
       }
     });
-    this.updatedFeatureTypeSubject.next({
-      ...this.updatedFeatureTypeSubject.value || {},
-      settings: {
-        ...settings,
-        hideAttributes: Array.from(hideAttributes),
-      },
-    });
-    this.saveEnabled = true;
+    this.updateSettings('hideAttributes', Array.from(hideAttributes));
   }
 
   public attributeOrderChanged(attributes: string[]) {
-    console.log(attributes);
+    this.updateSettings('attributeOrder', attributes);
+  }
+
+  public aliasesChanged(
+    originalSettings: FeatureTypeSettingsModel,
+    aliases: Array<{ attribute: string; alias: string | undefined }>,
+  ) {
+    const settings = this.updatedFeatureTypeSubject.value?.settings || {};
+    const attributeSettings: Record<string, AttributeSettingsModel> = { ...(settings?.attributeSettings || originalSettings.attributeSettings || {}) };
+    aliases.forEach(alias => {
+      attributeSettings[alias.attribute] = {
+        ...attributeSettings[alias.attribute],
+        title: alias.alias,
+      };
+    });
+    this.updateSettings('attributeSettings', attributeSettings);
+  }
+
+  private updateSettings(type: keyof FeatureTypeSettingsModel, value: any) {
     const settings = this.updatedFeatureTypeSubject.value?.settings || {};
     this.updatedFeatureTypeSubject.next({
       ...this.updatedFeatureTypeSubject.value || {},
-      settings: {
-        ...settings,
-        attributeOrder: attributes,
-      },
+      settings: { ...settings, [type]: value },
     });
     this.saveEnabled = true;
   }
