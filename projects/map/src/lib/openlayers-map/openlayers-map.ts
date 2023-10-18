0e51b3d8883b03f3d4b322c43611fd348881193e
Matthijs <matthijsln@gmail.com>
2023-10-19 01:39:55 +0200

    [HTM-932] Fix map shift for web mercator projection (#494)
    * Fix do not create OL View with a Projection that has extent set to max app map extent. This is wrong, but even if set to projection extent the EPSG:3857 latitude range from GeoTools EPSG database is [-20048966.1, 20048966.1], in degrees [-85.06, 85.06] not matching the OpenLayers range [-20037508.34, 20037508.34], in degrees [-90, 90], leading to map coordinate shift
    
    * Fix application of max extent
    
    * Fix API model (even though field is unused atm)
    
    * Do not set OL View resolutions

--- projects/map/src/lib/openlayers-map/openlayers-map.ts
+++ projects/map/src/lib/openlayers-map/openlayers-map.ts
@@ -56,15 +56,11 @@ export class OpenLayersMap implements MapViewerModel {
     }
 
     ProjectionsHelper.initProjection(options.projection, options.projectionDefinition, options.projectionAliases);
-    const projection = new Projection({
-      code: options.projection,
-      extent: options.maxExtent,
-    });
-    const resolutions = ProjectionsHelper.getResolutions(options.projection, options.maxExtent);
 
     const view = new View({
-      projection,
-      resolutions,
+      projection: options.projection,
+      extent: options.maxExtent,
+      showFullExtent: true,
     });
 
     const olMap = new OlMap({
@@ -80,7 +76,7 @@ export class OpenLayersMap implements MapViewerModel {
       collapsed: false,
     }));
 
-    this.initialExtent = options.initialExtent && options.initialExtent.length > 0
+    this.initialExtent = options.initialExtent?.length === 4
       ? options.initialExtent
       : options.maxExtent;
 
