4ae88bc5865c5850f6f2958bcd04708b69099e98
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-11-22 13:02:15 +0100

    Improved i18n extraction script

--- bin/extract-i18n.js
+++ bin/extract-i18n.js
@@ -14,24 +14,28 @@ const angularJsonPath = path.resolve(__dirname, '../angular.json');
 const angularJsonBackupPath = path.resolve(__dirname, '../angular.json.orig');
 
 const addTemporaryConfiguration = async (project) => {
-  const removeIdsWithPrefix = availableProjects
-    .filter(t => t !== project)
-    .map(p => `${p}.`);
   const configuration = {
     "outputPath": `projects/${project}/assets/locale`,
-    "removeIdsWithPrefix": removeIdsWithPrefix,
+    "includeIdsWithPrefix": [project],
     "sourceFile": `messages.${project}.en.xlf`,
     "targetFiles": [`messages.${project}.nl.xlf`]
   };
   try {
-    await fs.copyFile(angularJsonPath, angularJsonBackupPath);
     const angularJsonContents = JSON.parse((await fs.readFile(angularJsonPath)).toString());
     angularJsonContents['projects']['app']['architect']['extract-i18n']['configurations'][project] = configuration;
     await fs.writeFile(angularJsonPath, JSON.stringify(angularJsonContents));
   } catch (e) {
-    console.error('Could not make backup of angular.json');
+    console.error('Could not write i18n configuration to angular.json');
   }
 }
+
+const backupConfiguration = async () => {
+  try {
+    await fs.copyFile(angularJsonPath, angularJsonBackupPath);
+  } catch (e) {
+    console.error('Could not make backup of angular.json');
+  }
+};
 
 const cleanupTemporaryConfiguration = async () => {
   try {
@@ -50,11 +54,12 @@ const cleanupTemporaryConfiguration = async () => {
     const projects = singleProject && availableProjects.includes(singleProject)
       ? [singleProject]
       : availableProjects;
+    await backupConfiguration();
     for (const project of projects) {
       await addTemporaryConfiguration(project);
       await runCommand('ng', ['extract-i18n', `--configuration=${project}`], path.resolve(__dirname, '../'));
-      await cleanupTemporaryConfiguration();
     }
+    await cleanupTemporaryConfiguration();
   } catch (e) {
     console.log('Error occurred: ', e);
   }
