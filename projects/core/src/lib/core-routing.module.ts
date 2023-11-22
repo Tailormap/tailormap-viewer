bbfa58a3ea9f96032f21754e544742051cd42b2b
Matthijs Laan <matthijsln@gmail.com>
2023-11-22 12:41:56 +0100

    Navigate to part after missing locale suffix

--- projects/core/src/lib/core-routing.module.ts
+++ projects/core/src/lib/core-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Inject, LOCALE_ID, NgModule } from '@angular/core';
 import { NavigationError, Router, RouterModule, Routes } from '@angular/router';
 import { LoginComponent, ViewerAppComponent } from './pages';
 import { filter } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   // IMPORTANT: When you add a route, also add it to the FrontController class of tailormap-api, otherwise a user will get a 404 when
@@ -23,13 +24,27 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
 })
 export class CoreRoutingModule {
-  constructor(private router: Router) {
+  constructor(router: Router, @Inject(APP_BASE_HREF) baseHref: string, @Inject(LOCALE_ID) localeId: string) {
     // Use an alternative catch-all route instead of '**', this allows libraries to add routes
     router.events.pipe(
       takeUntilDestroyed(),
-      filter(event => event instanceof NavigationError),
-    ).subscribe(() => {
-      this.router.navigate(['app']);
+      filter((event): event is NavigationError => event instanceof NavigationError),
+    ).subscribe((event: NavigationError) => {
+      router.navigateByUrl(CoreRoutingModule.getErrorNavigationUrl(event.url, baseHref, localeId)); // We could navigate to route showing 404
     });
+  }
+
+  private static getErrorNavigationUrl(url: string, baseHref: string, localeId: string): string {
+    const localeSuffix = `/${localeId}/`;
+    // If the error URL starts with the base href but without the locale suffix, navigate to the part after that
+    // Example: /some-base/app/myapp, but the base href is /some-base/en/, navigate to /app/myapp
+    if(baseHref.endsWith(localeSuffix)) {
+      const baseHrefWithoutLocaleSuffix = baseHref.substring(0, baseHref.length - localeSuffix.length);
+      if (!url.startsWith(baseHrefWithoutLocaleSuffix)) {
+        return url.substring(baseHrefWithoutLocaleSuffix.length);
+      }
+    }
+    // Other route error URL, just go to /app/
+    return '/app/';
   }
 }
