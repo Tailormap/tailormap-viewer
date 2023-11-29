372bb947d9878cad72939c1fc78926164be87b82
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-11-29 15:41:47 +0100

    Fixed build + removed chalk dependency from postbuild script

--- projects/core/src/lib/components/components.module.ts
+++ projects/core/src/lib/components/components.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureInfoModule } from './feature-info/feature-info.module';
-import { MapControlsComponent } from './map-controls/map-controls.component';
 import { MenubarModule } from './menubar';
 import { TocModule } from './toc';
 import { ToolbarModule } from './toolbar';
@@ -12,22 +11,16 @@ import { AttributeListModule } from './attribute-list/attribute-list.module';
 import { PrintModule } from './print/print.module';
 import { FilterComponentModule } from './filter/filter-component.module';
 import { EditComponentModule } from './edit/edit-component.module';
-import { PanelComponentsComponent } from './panel-components/panel-components.component';
 import { RegisteredComponentsModule } from './registered-components-renderer/registered-components.module';
 
 @NgModule({
-  declarations: [
-    MapControlsComponent,
-    PanelComponentsComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
   ],
   exports: [
     FeatureInfoModule,
     MenubarModule,
-    MapControlsComponent,
-    PanelComponentsComponent,
     TocModule,
     ToolbarModule,
     LegendModule,
