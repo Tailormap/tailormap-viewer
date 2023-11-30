5e6dcc61679b460cf6f50160bc87c9eb36d966d4
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-11-30 20:15:56 +0100

    Add view orientation point style (#525)
    Note: this type of point style is not available in drawing component,
    but is intented for use to indicate a user position + orientation.

--- projects/map/src/lib/helpers/map-style.helper.ts
+++ projects/map/src/lib/helpers/map-style.helper.ts
@@ -181,6 +181,49 @@ export class MapStyleHelper {
     return label;
   }
 
+  private static getSvgIcon(props: {
+    svgContent: string;
+    symbolSize: number;
+    strokeWidth: number;
+    rotation?: number;
+    shrinkFactor?: number;
+  }) {
+    const svgStrokeWidth = 1 + (props.strokeWidth / 10);
+    const icon = [
+      `<svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">`,
+      ...props.svgContent,
+      '</svg>',
+    ].join('');
+    return new Icon({
+      src: 'data:image/svg+xml;base64,' + btoa(icon),
+      scale: (props.symbolSize + svgStrokeWidth) / (props.shrinkFactor || 6),
+      rotation: MapStyleHelper.getRotationForDegrees(props.rotation),
+    });
+  }
+
+  private static getRegularShape(props: {
+    symbolSize: number;
+    fillColor: string;
+    strokeColor?: string;
+    strokeWidth: number;
+    rotation?: number;
+    type: 'circle' | 'star' | 'square' | 'triangle' | 'diamond';
+  }) {
+    const POINT_SHAPES: Record<string, RegularShapeOptions> = {
+      circle: { points: Infinity, radius: props.symbolSize },
+      star: { points: 5, radius: props.symbolSize, radius2: props.symbolSize * .4, angle: 0 },
+      square: { points: 4, radius: props.symbolSize, angle: Math.PI / 4 },
+      triangle: { points: 3, radius: props.symbolSize, angle: 0 },
+      diamond: { points: 4, radius: props.symbolSize, angle: Math.PI / 2 },
+    };
+    return new RegularShape({
+      fill: new Fill({ color: props.fillColor  }),
+      stroke: props.strokeWidth === 0 || !props.strokeColor ? undefined : new Stroke({ color: props.strokeColor, width: props.strokeWidth }),
+      rotation: MapStyleHelper.getRotationForDegrees(props.rotation),
+      ...POINT_SHAPES[props.type],
+    });
+  }
+
   private static createShape(type: MapStylePointType, styleConfig: MapStyleModel): Style[] {
     if (type === 'label') {
       return [];
@@ -189,42 +232,35 @@ export class MapStyleHelper {
     const fillColor = styleConfig.pointFillColor || MapStyleHelper.DEFAULT_COLOR;
     const strokeColor = styleConfig.pointStrokeColor || MapStyleHelper.DEFAULT_COLOR;
     const strokeWidth = MapStyleHelper.getNumberValue(styleConfig.pointStrokeWidth, 1);
+    const rotation = styleConfig.pointRotation;
     if (type === 'cross' || type === 'arrow' || type === 'diamond') {
       const svgStrokeWidth = 1 + (strokeWidth / 10);
-      const path = type === 'arrow'
-        ? 'M0 6.75v-3.5h5.297V0L10 5l-4.703 5V6.75H0Z'
-        : type === 'diamond'
-          ? 'm5 0 3.5 4.997L5 10 1.5 4.997 5 0Z'
-          : 'M7.026 3V.015h-4V3H.005v4h3.021v3.006h4V7h2.969V3H7.026Z';
-      const icon = [
-        `<svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">`,
-        `<path d="${path}" fill="${fillColor}" stroke="${strokeColor}" stroke-width="${svgStrokeWidth}" />`,
-        '</svg>',
+      const paths = {
+        arrow: 'M0 6.75v-3.5h5.297V0L10 5l-4.703 5V6.75H0Z',
+        diamond: 'm5 0 3.5 4.997L5 10 1.5 4.997 5 0Z',
+        cross: 'M7.026 3V.015h-4V3H.005v4h3.021v3.006h4V7h2.969V3H7.026Z',
+      };
+      const svgContent = `<path d="${paths[type]}" fill="${fillColor}" stroke="${strokeColor}" stroke-width="${svgStrokeWidth}" />`;
+      return [new Style({ image: MapStyleHelper.getSvgIcon({ svgContent,  symbolSize,  rotation,  strokeWidth }) })];
+    }
+    if (type === 'view_orientation') {
+      const svgContent = [
+        '<defs>',
+        '<linearGradient id="gradient1" x1="0" x2="0" y1="0" y2="1">',
+        `<stop stop-color="${fillColor}" offset="60%" />`,
+        `<stop stop-color="${ColorHelper.getRgbStyleForColor(fillColor, .25)}" offset="100%" />`,
+        '</linearGradient>',
+        '</defs>',
+        '<path d="m5 4.021 2.366 4.997H2.634L5 4.021Z" fill="url(#gradient1)"/>',
       ].join('');
+      const viewDirectionIcon = MapStyleHelper.getSvgIcon({ svgContent,  symbolSize: symbolSize * 3,  rotation,  strokeWidth });
+      viewDirectionIcon.setAnchor([ 0.5, 0.175 ]);
       return [
-        new Style({
-          image: new Icon({
-            src: 'data:image/svg+xml;base64,' + btoa(icon),
-            scale: (symbolSize + svgStrokeWidth) / 6,
-            rotation: MapStyleHelper.getRotationForDegrees(styleConfig.pointRotation),
-          }),
-        }),
+        new Style({ image: MapStyleHelper.getRegularShape({ type: 'circle', fillColor, strokeColor, symbolSize, rotation, strokeWidth }) }),
+        new Style({ image: viewDirectionIcon }),
       ];
     }
-    const POINT_SHAPES: Record<string, RegularShapeOptions> = {
-      circle: { points: Infinity, radius: symbolSize },
-      star: { points: 5, radius: symbolSize, radius2: symbolSize * .4, angle: 0 },
-      square: { points: 4, radius: symbolSize, angle: Math.PI / 4 },
-      triangle: { points: 3, radius: symbolSize, angle: 0 },
-      diamond: { points: 4, radius: symbolSize, angle: Math.PI / 2 },
-    };
-    const baseShape = new RegularShape({
-      fill: new Fill({ color:fillColor  }),
-      stroke: strokeWidth === 0 || !strokeColor ? undefined : new Stroke({ color: strokeColor, width: strokeWidth }),
-      rotation: MapStyleHelper.getRotationForDegrees(styleConfig.pointRotation),
-      ...POINT_SHAPES[type],
-    });
-    return [new Style({ image: baseShape })];
+    return [new Style({ image: MapStyleHelper.getRegularShape({ type, strokeColor, strokeWidth, fillColor, rotation, symbolSize }) })];
   }
 
   private static createBuffer(feature: Feature<Geometry>, buffer: number, config: MapStyleModel) {
