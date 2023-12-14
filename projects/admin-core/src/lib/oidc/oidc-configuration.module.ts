74c52b081c186ebad62f9f0881af135575b1e37e
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-12-14 10:02:35 +0100

    HTM-961: Add settings page for global settings. Added OIDC to settings (#534)

--- projects/admin-core/src/lib/oidc/oidc-configuration.module.ts
+++ projects/admin-core/src/lib/oidc/oidc-configuration.module.ts
@@ -7,18 +7,40 @@ import { OIDCConfigurationEditSettingsComponent } from './oidc-configuration-edi
 import { OIDCConfigurationCreateComponent } from './oidc-configuration-create/oidc-configuration-create.component';
 import { OIDCConfigurationListComponent } from './oidc-configuration-list/oidc-configuration-list.component';
 import { SharedModule } from '@tailormap-viewer/shared';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { OIDCConfigurationEffects } from './state/oidc-configuration.effects';
 import { OIDCConfigurationFormComponent } from './oidc-configuration-form/oidc-configuration-form.component';
 import { SharedAdminComponentsModule } from '../shared/components/shared-admin-components.module';
 import { OIDCConfigurationHomeComponent } from './oidc-configuration-home/oidc-configuration-home.component';
 import { OIDCConfigurationEditComponent } from './oidc-configuration-edit/oidc-configuration-edit.component';
-import { CatalogModule } from '../catalog/catalog.module';
 import { OIDCConfigurationService } from './services/oidc-configuration.service';
+import { OIDCConfigurationPageComponent } from './oidc-configuration/oidc-configuration-page.component';
+import { Routes as AdminRoutes } from '../routes';
+import { AdminSettingsRouterService } from '../settings/services/admin-settings-router.service';
+
+const routes: Routes = [{
+  path: AdminRoutes.OIDC_CONFIGURATION,
+  component: OIDCConfigurationPageComponent,
+  children: [
+    {
+      path: '',
+      component: OIDCConfigurationHomeComponent,
+    },
+    {
+      path: AdminRoutes.OIDC_CONFIGURATION_CREATE,
+      component: OIDCConfigurationCreateComponent,
+    },
+    {
+      path: AdminRoutes.OIDC_CONFIGURATION_DETAILS,
+      component: OIDCConfigurationEditComponent,
+    },
+  ],
+}];
 
 @NgModule({
   declarations: [
+    OIDCConfigurationPageComponent,
     OIDCConfigurationEditSettingsComponent,
     OIDCConfigurationCreateComponent,
     OIDCConfigurationListComponent,
@@ -33,14 +55,18 @@ import { OIDCConfigurationService } from './services/oidc-configuration.service'
     EffectsModule.forFeature([OIDCConfigurationEffects]),
     SharedAdminComponentsModule,
     RouterOutlet,
-    CatalogModule,
  ],
   exports: [
+    OIDCConfigurationPageComponent,
     OIDCConfigurationListComponent,
   ],
 })
 export class OIDCConfigurationModule {
-  constructor(oidcConfigurationService: OIDCConfigurationService) {
+  constructor(
+    oidcConfigurationService: OIDCConfigurationService,
+    adminSettingsRouterService: AdminSettingsRouterService,
+  ) {
     oidcConfigurationService.listenForOIDCConfigurationChanges();
+    adminSettingsRouterService.registerSettingsRoutes(routes);
   }
 }
