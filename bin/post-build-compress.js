89e008d7db93ceb5b82d9d605b23e03a6e99adf2
Matthijs Laan <matthijsln@gmail.com>
2024-02-08 14:44:39 +0100

    Remove Brotli compression to speed up build

--- bin/post-build-compress.js
+++ bin/post-build-compress.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-const zlib = require("zlib");
+const zlib = require('zlib');
 const chalk = require('chalk');
 
 const EXTENSIONS = ['.js', '.css', '.svg', '.map'];
 const COMPRESS_SIZE_THRESHOLD = 1024;
-const BR_VS_GZ_SIZE_THRESHOLD = 500;
 
 // recursive option requires Node v20
 const files = fs.readdirSync(path.resolve(__dirname, '../dist/app/'), { recursive: true, withFileTypes: true })
@@ -23,45 +22,19 @@ let compressedTotal = 0;
 files.forEach(entry => {
   const { size } = fs.statSync(entry.filename);
   if (size > COMPRESS_SIZE_THRESHOLD) {
-    const isSourceMap = path.extname(entry.filename) === '.map';
-
-    process.stdout.write(`Compressing ${entry.displayName.padEnd(displayNamePad, ' ')} size ${(size + "").padStart(7, ' ')}... `);
+    process.stdout.write(`Compressing ${entry.displayName.padEnd(displayNamePad, ' ')} size ${(size + "").padStart(7, ' ')}, ratio: `);
 
     const contents = fs.readFileSync(entry.filename);
 
     const gzipped = zlib.gzipSync(contents, { level: 9 });
     fs.writeFileSync(entry.filename + ".gz", gzipped);
 
-    let writeBrotli = false;
-    let brotliLength = 0;
-    let gzipMinusBrotliLength = 0;
-    // Don't waste time compressing source maps with Brotli
-    if (!isSourceMap) {
-      const brotli = zlib.brotliCompressSync(contents, {
-        params: {
-          [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY
-        }
-      });
+    const ratio = (gzipped.length / size).toFixed(2);
+    process.stdout.write(` ${chalk.bold(ratio)}\n`);
 
-      brotliLength = brotli.length;
-      gzipMinusBrotliLength = gzipped.length - brotli.length;
-      writeBrotli = gzipMinusBrotliLength > BR_VS_GZ_SIZE_THRESHOLD;
-      if (writeBrotli) {
-        fs.writeFileSync(entry.filename + ".br", brotli);
-      }
-    }
-
-    const gzRatio = (gzipped.length / size).toFixed(2);
-    if (!isSourceMap) {
-      const brRatio = (brotliLength / size).toFixed(2);
-      const chalkFn = writeBrotli ? chalk.green : chalk.gray;
-      process.stdout.write(`gzip ${chalk.bold(gzRatio)}, br ${chalkFn(chalk.bold(brRatio))}, br > gzip: ${chalkFn((gzipMinusBrotliLength + "").padStart(5, ' '))}\n`);
-    } else {
-      process.stdout.write(`gzip ${chalk.bold(gzRatio)}\n`);
-    }
-    if (entry.displayName.startsWith('app/en/') && !isSourceMap) {
+    if (entry.displayName.startsWith('app/en/') && path.extname(entry.filename) !== '.map') {
       bundleTotal += size;
-      compressedTotal += (writeBrotli ? brotliLength : gzipped.length);
+      compressedTotal += gzipped.length;
     }
   }
 });
