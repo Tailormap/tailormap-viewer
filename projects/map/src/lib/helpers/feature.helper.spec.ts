f4022908aa0697cc0bf057e6415f8c152f3dc5e4
Matthijs Laan <matthijsln@gmail.com>
2024-02-13 18:07:18 +0100

    HTM-962: Override GeoTools' projection WKT with proj4js definition for EPSG:28992

--- projects/map/src/lib/helpers/feature.helper.spec.ts
+++ projects/map/src/lib/helpers/feature.helper.spec.ts
@@ -51,7 +51,7 @@ describe('FeatureTypesHelper', () => {
   test('checks transformGeometry polygon from EPSG:28992 to EPSG:3857', () => {
     ProjectionsHelper.initProjection('EPSG:28992',
       // eslint-disable-next-line max-len
-      '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs');
+      '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs');
     // eslint-disable-next-line max-len
     const wktGeom = 'POLYGON ((131104 460577, 129823 461804, 129495 462130, 128732 461629, 128944 461027, 129420 460143, 129722 459834, 129823 459563, 130115 459450, 130698 458799, 130791 458756, 131164 459552, 131574 460125, 131104 460577))';
     // transformed to EPSG:3857 using postgis
@@ -60,7 +60,7 @@ describe('FeatureTypesHelper', () => {
     //    558608.5089436477 6822965.239498022,558774.9233316716 6822524.435377617,559250.4864285775 6822342.685742314,
     //    560203.2990491522 6821286.592399324,560354.787083448 6821217.268622127,560954.7970522561 6822517.280664146,
     //    561616.8607501892 6823454.284349732,560849.2581363895 6824187.345771471))
-    expect(FeatureHelper.transformGeometry(wktGeom, 'EPSG:28992', 'EPSG:3857')).toContain('POLYGON((560849.2581');
+    expect(FeatureHelper.transformGeometry(wktGeom, 'EPSG:28992', 'EPSG:3857')).toContain('POLYGON((560849.2660');
     expect(FeatureHelper.transformGeometry(wktGeom, 'EPSG:28992', 'EPSG:3857')).toContain(' 6824187.345');
   });
 });
