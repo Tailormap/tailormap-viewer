2d8a2cb3d9a290aa2a3789a405c6801fb8bdbdfc
Matthijs Laan <matthijsln@gmail.com>
2024-04-05 14:50:32 +0200

    Fix tests

--- projects/core/src/lib/components/toolbar/share-viewer/share-viewer.component.spec.ts
+++ projects/core/src/lib/components/toolbar/share-viewer/share-viewer.component.spec.ts
@@ -2,13 +2,21 @@ import { render, screen } from '@testing-library/angular';
 import { ShareViewerComponent } from './share-viewer.component';
 import { SharedModule } from '@tailormap-viewer/shared';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { selectShowLoginButton, selectUserDetails, selectUserIsAdmin } from '../../../state/core.selectors';
 
 describe('ShareViewerComponent', () => {
 
   test('should render', async () => {
     await render(ShareViewerComponent, {
       imports: [ SharedModule, MatIconTestingModule ],
-      providers: [],
+      providers: [
+        provideMockStore({
+          selectors: [
+            { selector: selectUserIsAdmin, value: true },
+          ],
+        }),
+      ],
     });
     expect(screen.getByLabelText('Share viewer'));
   });
