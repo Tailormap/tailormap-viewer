597701c0fa34d9e79bd3afdea6b9105290cbaaa5
Matthijs Laan <matthijsln@gmail.com>
2024-06-11 23:18:03 +0200

    Override legend image with uploaded image

--- projects/admin-core/src/lib/catalog/layer-settings-form/layer-settings-form.component.ts
+++ projects/admin-core/src/lib/catalog/layer-settings-form/layer-settings-form.component.ts
@@ -93,6 +93,7 @@ export class LayerSettingsFormComponent implements OnInit {
     title: new FormControl('', { nonNullable: true }),
     description: new FormControl<string | null>(null),
     attribution: new FormControl<string | null>(null),
+    legendImageId: new FormControl<string | null>(null),
     featureSourceId: new FormControl<number | null>(null),
     featureTypeName: new FormControl<string | null>(null),
     hiDpiEnabled: new FormControl<boolean | null>(null),
@@ -131,6 +132,7 @@ export class LayerSettingsFormComponent implements OnInit {
     const settings: LayerSettingsModel = {
       hiDpiDisabled: LayerSettingsFormComponent.getInverseBooleanOrDefault(value.hiDpiEnabled, undefined),
       attribution: value.attribution || undefined,
+      legendImageId: value.legendImageId || undefined,
       description: value.description || undefined,
     };
     if (this.isLayerSpecific) {
@@ -177,6 +179,7 @@ export class LayerSettingsFormComponent implements OnInit {
       [ values.title, this._layerSettings.title ],
       [ values.description, this._layerSettings.description ],
       [ values.attribution, this._layerSettings.attribution ],
+      [ values.legendImageId, this._layerSettings.legendImageId ],
       [ values.hiDpiDisabled, this._layerSettings.hiDpiDisabled ],
       [ values.hiDpiMode, this._layerSettings.hiDpiMode ],
       [ values.hiDpiSubstituteLayer, this._layerSettings.hiDpiSubstituteLayer ],
@@ -209,6 +212,7 @@ export class LayerSettingsFormComponent implements OnInit {
       title: this.layerSettings?.title ? this.layerSettings.title : '',
       description: this.layerSettings?.description || null,
       attribution: this.layerSettings?.attribution || null,
+      legendImageId: this.layerSettings?.legendImageId || null,
       featureSourceId: this.layerSettings?.featureType?.featureSourceId || null,
       featureTypeName: this.layerSettings?.featureType?.featureTypeName || null,
       hiDpiEnabled,
@@ -276,4 +280,7 @@ export class LayerSettingsFormComponent implements OnInit {
     return !!settings && this.isXYZ;
   }
 
+  public onLegendImageChanged($event: string) {
+    this.layerSettingsForm.patchValue({ legendImageId: $event});
+  }
 }
