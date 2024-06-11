2d6fe1b401d01a5efed865f922dafd2d0fbff925
Matthijs Laan <matthijsln@gmail.com>
2024-06-11 20:07:14 +0200

    Display uploaded raster legend images always as hi-dpi by scaling down

--- projects/core/src/lib/components/legend/legend-layer/legend-layer.component.ts
+++ projects/core/src/lib/components/legend/legend-layer/legend-layer.component.ts
@@ -21,6 +21,8 @@ export class LegendLayerComponent implements OnChanges {
 
   public srcset = '';
 
+  public scaleHiDpiImage = false;
+
   public failedToLoadMessage = $localize `:@@core.legend.failed-loading-legend:Failed to load legend for`;
 
   public ngOnChanges() {
@@ -30,7 +32,12 @@ export class LegendLayerComponent implements OnChanges {
     this.urlWithOptions = this.legendInfo.url;
     this.srcset = '';
 
-    if (this.legendInfo.layer.service?.serverType === ServerType.GEOSERVER
+    if(this.legendInfo.url.includes('/uploads/legend/') && !this.legendInfo.url.endsWith(".svg")) {
+      // Currently, the uploaded image must always be hi-dpi. We don't have two versions (automatically downscaled or separately uploaded)
+      // so only hi-dpi. Make sure with CSS transform { scale(0.5) } the image is displayed downscaled on devicePixelRatio = 1 screens as
+      // well. Don't downscale SVG images though (checked by extension).
+      this.scaleHiDpiImage = true;
+    } else if (this.legendInfo.layer.service?.serverType === ServerType.GEOSERVER
       && LegendService.isGetLegendGraphicRequest(this.legendInfo.url)) {
       const legendOptions: GeoServerLegendOptions = {
         fontAntiAliasing: true,
