93c73737217d6b04a0294b13addfae3ce94ebd35
Geert Plaisier <geert.plaisier@gnamic.nl>
2024-07-10 12:03:41 +0200

    HTM-1143: Select a search index for an application layer  (#669)
    Co-authored-by: Mark Prins <1165786+mprins@users.noreply.github.com>

--- projects/admin-core/src/lib/application/application-layer-settings/application-layer-settings.component.ts
+++ projects/admin-core/src/lib/application/application-layer-settings/application-layer-settings.component.ts
@@ -1,20 +1,17 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { AppLayerSettingsModel, AppTreeLayerNodeModel, FeatureTypeModel, FormModel, FormSummaryModel } from '@tailormap-admin/admin-api';
+import {
+  AppLayerSettingsModel, AppTreeLayerNodeModel, FeatureTypeModel, FormModel, FormSummaryModel, SearchIndexModel,
+} from '@tailormap-admin/admin-api';
 import { Store } from '@ngrx/store';
 import { selectSelectedApplicationLayerSettings } from '../state/application.selectors';
 import {
-  BehaviorSubject, combineLatest, concatMap, debounceTime, distinctUntilChanged, map, Observable, of, startWith, Subject, switchMap, take,
+  BehaviorSubject, combineLatest, debounceTime, distinctUntilChanged, map, Observable, of, startWith, Subject, switchMap, take,
   takeUntil,
 } from 'rxjs';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LoadingStateEnum, TreeModel } from '@tailormap-viewer/shared';
-import { ExtendedGeoServiceModel } from '../../catalog/models/extended-geo-service.model';
-import { ExtendedGeoServiceLayerModel } from '../../catalog/models/extended-geo-service-layer.model';
 import { ExtendedGeoServiceAndLayerModel } from '../../catalog/models/extended-geo-service-and-layer.model';
-import { GeoServiceFormDialogComponent } from '../../catalog/geo-service-form-dialog/geo-service-form-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { AdminSnackbarService } from '../../shared/services/admin-snackbar.service';
-import { GeoServiceLayerFormDialogComponent } from '../../catalog/geo-service-layer-form-dialog/geo-service-layer-form-dialog.component';
 import { ExtendedFeatureTypeModel } from '../../catalog/models/extended-feature-type.model';
 import {
   selectFeatureSourceAndFeatureTypesById,
@@ -24,10 +21,11 @@ import {
 } from '../application-layer-attribute-settings/application-layer-attribute-settings.component';
 import { ExtendedFeatureSourceModel } from '../../catalog/models/extended-feature-source.model';
 import { FeatureSourceService } from '../../catalog/services/feature-source.service';
-import { GeoServiceService } from '../../catalog/services/geo-service.service';
 import { selectFormsForFeatureType, selectFormsLoadStatus } from '../../form/state/form.selectors';
 import { loadForms } from '../../form/state/form.actions';
 import { FormService } from '../../form/services/form.service';
+import { selectSearchIndexesForFeatureType, selectSearchIndexesLoadStatus } from '../../search-index/state/search-index.selectors';
+import { loadSearchIndexes } from '../../search-index/state/search-index.actions';
 
 type FeatureSourceAndType = {
   featureSource: ExtendedFeatureSourceModel;
@@ -81,6 +79,9 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
   private selectableFormsSubject$ = new BehaviorSubject<FormSummaryModel[]>([]);
   public selectableForms$ = this.selectableFormsSubject$.asObservable();
 
+  private selectableSearchIndexesSubject$ = new BehaviorSubject<SearchIndexModel[]>([]);
+  public selectableSearchIndexes$ = this.selectableSearchIndexesSubject$.asObservable();
+
   public editableTooltipSubject$ = new BehaviorSubject(this.editingDisabledTooltip);
   public editableTooltip$ = this.editableTooltipSubject$.asObservable();
 
@@ -94,6 +95,7 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
     description: new FormControl<string | null>(null),
     editable: new FormControl<boolean>(false),
     formId: new FormControl<number | null>(null),
+    searchIndexId: new FormControl<number | null>(null),
     autoRefreshInSeconds: new FormControl<number | null>(null),
   });
 
@@ -102,8 +104,6 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
   constructor(
     private store$: Store,
     private dialog: MatDialog,
-    private adminSnackbarService: AdminSnackbarService,
-    private geoServiceService: GeoServiceService,
     private featureSourceService: FeatureSourceService,
     private formService: FormService,
   ) { }
@@ -133,6 +133,7 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
           description: value.description,
           editable: value.editable ?? undefined,
           formId: value.formId ?? null,
+          searchIndexId: value.searchIndexId ?? null,
           autoRefreshInSeconds: value.autoRefreshInSeconds ?? null,
         };
         this.layerSettingsChange.emit({ nodeId: this.node.id, settings });
@@ -146,6 +147,14 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
         }
       });
 
+    this.store$.select(selectSearchIndexesLoadStatus)
+      .pipe(take(1))
+      .subscribe(loadStatus => {
+        if (loadStatus === LoadingStateEnum.INITIAL || loadStatus === LoadingStateEnum.FAILED) {
+          this.store$.dispatch(loadSearchIndexes());
+        }
+      });
+
     this.featureSourceAndType$
       .pipe(takeUntil(this.destroyed))
       .subscribe((fs) => {
@@ -156,13 +165,21 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
       .pipe(
         takeUntil(this.destroyed),
         switchMap(fs => {
-          return !fs
-            ? of([])
-            : this.store$.select(selectFormsForFeatureType(fs.featureSource.id, fs.featureType?.name));
+          return !fs || !fs.featureType
+            ? of([ null, null ])
+            : combineLatest([
+              this.store$.select(selectFormsForFeatureType(fs.featureSource.id, fs.featureType.name)),
+              this.store$.select(selectSearchIndexesForFeatureType(fs.featureType.originalId)),
+            ]);
         }),
       )
-      .subscribe(forms => {
-        this.selectableFormsSubject$.next(forms);
+      .subscribe(([ forms, searchIndexes ]) => {
+        if (forms) {
+          this.selectableFormsSubject$.next(forms);
+        }
+        if (searchIndexes) {
+          this.selectableSearchIndexesSubject$.next(searchIndexes);
+        }
       });
 
     const formIdControl = this.layerSettingsForm.get('formId');
@@ -225,49 +242,9 @@ export class ApplicationLayerSettingsComponent implements OnInit, OnDestroy {
       description: nodeSettings.description || null,
       editable: nodeSettings.editable ?? false,
       formId: nodeSettings.formId || null,
+      searchIndexId: nodeSettings.searchIndexId || null,
       autoRefreshInSeconds: nodeSettings.autoRefreshInSeconds || null,
     }, { emitEvent: false });
-  }
-
-  public updateGeoServiceSetting($event: MouseEvent, geoService: ExtendedGeoServiceModel) {
-    $event.preventDefault();
-    this.geoServiceService.getDraftGeoService$(geoService.id)
-      .pipe(
-        take(1),
-        concatMap(service => {
-          if (!service) {
-            return of(null);
-          }
-          return GeoServiceFormDialogComponent.open(this.dialog, {
-            geoService: service,
-            parentNode: geoService.catalogNodeId,
-          }).afterClosed();
-        }),
-        takeUntil(this.destroyed),
-      )
-      .subscribe(updatedService => {
-        if (updatedService) {
-          this.adminSnackbarService.showMessage($localize `:@@admin-core.application.service-updated:Service ${updatedService.title} updated`);
-        }
-      });
-  }
-
-  public updateGeoServiceLayerSetting($event: MouseEvent, geoService: ExtendedGeoServiceModel, geoServiceLayer: ExtendedGeoServiceLayerModel) {
-    $event.preventDefault();
-    GeoServiceLayerFormDialogComponent.open(this.dialog, {
-      geoService,
-      geoServiceLayer,
-    }).afterClosed().pipe(takeUntil(this.destroyed)).subscribe(updatedSettings => {
-      if (updatedSettings) {
-        this.adminSnackbarService.showMessage($localize `:@@admin-core.application.layer-settings-updated:Layer settings updated`);
-      }
-    });
-  }
-
-  public updateFeatureTypeSetting(updatedFeatureType: FeatureTypeModel) {
-    if (updatedFeatureType) {
-      this.adminSnackbarService.showMessage($localize `:@@admin-core.feature-type-settings-updated:Feature type settings updated`);
-    }
   }
 
   private initFeatureSource(serviceLayer: ExtendedGeoServiceAndLayerModel | null) {
---
+++ projects/api/src/lib/models/layer-search-index.model.ts
@@ -0,0 +1,4 @@
+export interface LayerSearchIndexModel {
+  id: number;
+  name: string;
+}
