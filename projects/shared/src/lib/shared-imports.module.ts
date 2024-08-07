fec89769986ae351e85a6d4056b4e9e14ef21d4a
Geert Plaisier <geert.plaisier@gnamic.nl>
2024-08-07 15:11:57 +0200

    Updated Angular @ 18

--- projects/shared/src/lib/shared-imports.module.ts
+++ projects/shared/src/lib/shared-imports.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
