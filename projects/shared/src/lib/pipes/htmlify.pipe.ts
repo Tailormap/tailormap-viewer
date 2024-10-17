b81070ecde713154b5e68d609b49dab68c9f3f0c
Geert Plaisier <geert.plaisier@gnamic.nl>
2024-10-17 17:37:27 +0200

    HTM-816: Escape HTML in layer attributions (#725)

--- projects/shared/src/lib/pipes/htmlify.pipe.ts
+++ projects/shared/src/lib/pipes/htmlify.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { HtmlifyConstants } from './htmlify.constants';
+import { HtmlifyHelper } from '../helpers/htmlify.helper';
 
 @Pipe({
   name: 'htmlify',
@@ -9,42 +9,11 @@ export class HtmlifyPipe implements PipeTransform {
 
   public constructor(private sanitizer: DomSanitizer) {}
 
-  private static readonly URL_REGEXP = new RegExp(`${HtmlifyConstants.MD_PART}|${HtmlifyConstants.URL_PART}`, 'ig');
-  private static readonly IMG_REGEXP = /\.(jpg|jpeg|png|webp|svg|gif)/i;
-  private static readonly VENDOR_SPECIFIC_IMAGE_REGEXP = /getimage\.ashx/i;
-  private static readonly NEWLINE_REGEXP = /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g;
-
-  private static linkReplacer(match: string): string {
-    if (match[0] === '[') { // safe to do since this is already matched using Regex, only MD URL will start with [
-      const url = match.substring(match.indexOf('(') + 1, match.length - 1);
-      const name = match.substring(1, match.indexOf(']'));
-      return `<a href="${url}" target="_blank">${name}</a>`;
-    }
-    if (HtmlifyPipe.IMG_REGEXP.test(match)
-      || HtmlifyPipe.VENDOR_SPECIFIC_IMAGE_REGEXP.test(match)) {
-      return `<a href="${match}" target="_blank"><img src="${match}" alt="${match}" /></a>`;
-    }
-    return `<a href="${match}" target="_blank">${match}</a>`;
-  }
-
   public transform(value: string | null): SafeHtml | null {
     if (typeof value === 'string') {
-      const sanitizedHTML = HtmlifyPipe.escapeText(value);
-      if (!sanitizedHTML) {
-        return sanitizedHTML;
-      }
-      const replaceUrls = sanitizedHTML.replace(HtmlifyPipe.URL_REGEXP, HtmlifyPipe.linkReplacer);
-      const replacedBreaks = replaceUrls.replace(HtmlifyPipe.NEWLINE_REGEXP, '$1<br />$2');
-      return this.sanitizer.bypassSecurityTrustHtml(replacedBreaks);
+      return this.sanitizer.bypassSecurityTrustHtml(HtmlifyHelper.htmlifyContents(value));
     }
     return value;
-  }
-
-  private static escapeText(text: string) {
-    return text
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
   }
 
 }
