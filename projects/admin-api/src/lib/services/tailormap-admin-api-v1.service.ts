1e945cf68ba91be944260a7605a827ce74110e6f
Matthijs Laan <matthijsln@gmail.com>
2024-10-17 17:39:08 +0200

    HTM-1260: Check Solr availability and show error if not available

--- projects/admin-api/src/lib/services/tailormap-admin-api-v1.service.ts
+++ projects/admin-api/src/lib/services/tailormap-admin-api-v1.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { TailormapAdminApiV1ServiceModel } from './tailormap-admin-api-v1-service.model';
 import { catchError, map, Observable, of } from 'rxjs';
 import {
   CatalogNodeModel, GeoServiceModel, GeoServiceWithLayersModel, GroupModel, FeatureSourceModel, UserModel, ApplicationModel, ConfigModel,
   OIDCConfigurationModel, FeatureTypeModel,
   GeoServiceSummaryWithLayersModel, FeatureSourceSummaryWithFeatureTypesModel, FormSummaryModel, FormModel, UploadModel, SearchIndexModel,
+  SearchIndexPingResponseModel,
 } from '../models';
 import { CatalogModelHelper } from '../helpers/catalog-model.helper';
 import { TailormapApiConstants } from '@tailormap-viewer/api';
@@ -318,6 +319,19 @@ export class TailormapAdminApiV1Service implements TailormapAdminApiV1ServiceMod
       observe: 'response',
     }).pipe(
       map(response => response.status === 204),
+    );
+  }
+
+  public pingSearchIndexEngine$(): Observable<SearchIndexPingResponseModel> {
+    return this.httpClient.get<{ status: string }>(`${TailormapAdminApiV1Service.BASE_URL}/index/ping`).pipe(
+      map(response => ({ success: true, ...response })),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 500 && err.error.code !== undefined && err.error.message !== undefined) {
+          return of({ success: false, code: err.error.code, message: err.error.message });
+        } else {
+          return of({ success: false, code: err.status, message: err.statusText });
+        }
+      }),
     );
   }
 
