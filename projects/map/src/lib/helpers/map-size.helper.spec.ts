150ad121d4c5b8371b110860e4004316a586f16a
Geert Plaisier <geert.plaisier@gnamic.nl>
2024-10-23 18:34:38 +0200

    HTM-1275: Measure tool adjusted distance threshold from 100m to 1000m (#734)

--- projects/map/src/lib/helpers/map-size.helper.spec.ts
+++ projects/map/src/lib/helpers/map-size.helper.spec.ts
@@ -6,7 +6,7 @@ describe('MapSizeHelper', () => {
     expect(MapSizeHelper.getFormattedLength()).toEqual('');
     expect(MapSizeHelper.getFormattedLength(100)).toEqual('100 m');
     expect(MapSizeHelper.getFormattedLength(10)).toEqual('10 m');
-    expect(MapSizeHelper.getFormattedLength(101)).toEqual('0.1 km');
+    expect(MapSizeHelper.getFormattedLength(101)).toEqual('101 m');
     expect(MapSizeHelper.getFormattedLength(1201)).toEqual('1.2 km');
     expect(MapSizeHelper.getFormattedLength(50000)).toEqual('50 km');
   });
@@ -15,8 +15,8 @@ describe('MapSizeHelper', () => {
     expect(MapSizeHelper.getFormattedArea()).toEqual('');
     expect(MapSizeHelper.getFormattedArea(100)).toEqual('100 m');
     expect(MapSizeHelper.getFormattedArea(10000)).toEqual('10000 m');
-    expect(MapSizeHelper.getFormattedArea(10001)).toEqual('0.01 km');
-    expect(MapSizeHelper.getFormattedArea(50000)).toEqual('0.05 km');
+    expect(MapSizeHelper.getFormattedArea(10001)).toEqual('10001 m');
+    expect(MapSizeHelper.getFormattedArea(50000)).toEqual('50000 m');
     expect(MapSizeHelper.getFormattedArea(5000000)).toEqual('5 km');
   });
 
