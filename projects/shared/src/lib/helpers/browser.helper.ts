81c1022f81b1e9ef66a702d54959b2c6a36267e5
Geert Plaisier <geert.plaisier@gnamic.nl>
2024-10-23 16:08:46 +0200

    HTM-1100: Add overlay when scrolling on map when TM is in an iframe (#733)

--- projects/shared/src/lib/helpers/browser.helper.ts
+++ projects/shared/src/lib/helpers/browser.helper.ts
@@ -2,6 +2,7 @@ export class BrowserHelper {
   public static isTouchDevice = ('ontouchstart' in window) && (navigator.maxTouchPoints > 0);
   public static isMobile = window.matchMedia ? !window.matchMedia('(min-device-width: 1200px)').matches : false;
   public static isHiDPI = window.devicePixelRatio > 1;
+  public static isMac = navigator.userAgent.indexOf("Mac") > -1;
   public static getScreenWith = () => window.innerWidth;
   public static getScreenHeight = () => window.innerHeight;
 }
