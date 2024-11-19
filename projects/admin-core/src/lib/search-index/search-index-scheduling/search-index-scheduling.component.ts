5e9007237e2e494505339d7411f54b47796b8204
steinkobben <steinkobben1@gmail.com>
2024-11-19 08:55:10 +0100

    add form fields

--- projects/admin-core/src/lib/search-index/search-index-scheduling/search-index-scheduling.component.ts
+++ projects/admin-core/src/lib/search-index/search-index-scheduling/search-index-scheduling.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Destr
 import { FormControl, FormGroup } from '@angular/forms';
 import { SearchIndexModel, TaskSchedule } from '@tailormap-admin/admin-api';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { debounceTime } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'tm-admin-search-index-scheduling',
@@ -12,11 +12,19 @@ import { debounceTime } from 'rxjs';
 })
 export class SearchIndexSchedulingComponent implements OnInit {
 
+  public taskSchedule: TaskSchedule | undefined = undefined;
+
   @Input({ required: true })
-  public searchIndex: SearchIndexModel | undefined;
+  public set searchIndex(form: SearchIndexModel | null) {
+    this.taskSchedule = form?.schedule;
+    this.initForm(form?.schedule);
+  }
 
   @Output()
   public updateSchedule = new EventEmitter<{ searchIndex: Pick<SearchIndexModel, 'schedule'> }>();
+
+  @Output()
+  public formChanged = new EventEmitter<boolean>();
 
   public scheduleOptions = [
     {cronExpression: '', viewValue: 'No schedule'},
@@ -32,6 +40,8 @@ export class SearchIndexSchedulingComponent implements OnInit {
 
   public scheduleForm = new FormGroup({
     cronExpression: new FormControl(),
+    description: new FormControl(),
+    priority: new FormControl(),
   });
 
   public ngOnInit(): void {
@@ -39,17 +49,40 @@ export class SearchIndexSchedulingComponent implements OnInit {
       .pipe(
         takeUntilDestroyed(this.destroyRef),
         debounceTime(250),
+        distinctUntilChanged(),
+      )
+      .subscribe(validForm => {
+        this.formChanged.emit(true);
+      });
+    this.scheduleForm.valueChanges
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        debounceTime(250),
       )
       .subscribe(value => {
         const schedule: TaskSchedule = {
-          ...this.searchIndex?.schedule,
+          ...this.taskSchedule,
           cronExpression: value.cronExpression,
+          description: value.description,
+          priority: value.priority,
         };
         const searchIndex: Pick<SearchIndexModel, 'schedule'> = {
           schedule: schedule,
         };
         this.updateSchedule.emit({ searchIndex });
       });
+  }
+
+  private initForm(schedule: TaskSchedule | undefined) {
+    if (!schedule) {
+      this.scheduleForm.patchValue({ cronExpression: '', description: '', priority: '' }, { emitEvent: false });
+    } else {
+      this.scheduleForm.patchValue({
+        cronExpression: schedule.cronExpression,
+        description: schedule.description,
+        priority: schedule.priority,
+      }, { emitEvent: false });
+    }
   }
 
 }
