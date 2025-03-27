3c1fbea3a201d3229dc9055ad7432c846670d46e
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-03-27 11:13:27 +0100

    HTM-1432: Lazy load the QR code library

--- projects/core/src/lib/shared/helpers/image.helper.ts
+++ projects/core/src/lib/shared/helpers/image.helper.ts
@@ -1,5 +1,4 @@
-import { Observable, of, Subject } from 'rxjs';
-import { Byte, Charset, Encoder } from '@nuintun/qrcode';
+import { from, map, Observable, Subject } from 'rxjs';
 
 export class ImageHelper {
   public static imageUrlToPng$(imageUrl: string): Observable<{ imageData: string; width: number; height: number }> {
@@ -36,21 +35,33 @@ export class ImageHelper {
     return subject.asObservable();
   }
 
+  /**
+   * Convert a string to a base64 encoded QR code image.
+   * This can be used to create QR codes for any text dat, the size of the QR image is set automatically,
+   * the amount of redundancy is set to 'M', this means you can have about 2331 bytes of data and allows for about 15% loss recovery,
+   * assuming the `asciiData` string is ISO/IEC 8859-1 encoded. The larger the input the larger the generated image.
+   *
+   * @param asciiData The data to encode in the QR code
+   * @param foreground The color of the QR code
+   * @param background The background color of the QR code
+   * @returns An observable that emits the base64 encoded QR code GIF image and the size of the image
+   */
   public static string2Base64QRcode$(asciiData: string, foreground: string, background: string): Observable<{
-    imageData: string;
-    widthPx: number;
-    heightPx: number;
+    imageData: string; widthPx: number; heightPx: number;
   }> {
-    const encoder = new Encoder({
-      level: 'M', version: 'Auto', hints: { fnc1: ['GS1'] },
-    });
-    const qrcode = encoder.encode(new Byte(asciiData, Charset.ASCII));
 
-    return of({
-      imageData: qrcode.toDataURL(1, {
-        margin: 1, foreground: this.hex2rgb(foreground), background: this.hex2rgb(background),
-      }), widthPx: qrcode.size, heightPx: qrcode.size,
-    });
+    return from(import('@nuintun/qrcode')).pipe(map(({ Encoder, Byte, Charset }) => {
+      const encoder = new Encoder({
+        level: 'M', version: 'Auto', hints: { fnc1: ['GS1'] },
+      });
+      const qrcode = encoder.encode(new Byte(asciiData, Charset.ASCII));
+
+      return {
+        imageData: qrcode.toDataURL(1, {
+          margin: 1, foreground: this.hex2rgb(foreground), background: this.hex2rgb(background),
+        }), widthPx: qrcode.size, heightPx: qrcode.size,
+      };
+    }));
   }
 
   private static hex2rgb(hex: string): [R: number, G: number, B: number] {
