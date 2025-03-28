3509705a0892b8944825cbf902a01ab62ffb4810
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-03-28 10:53:07 +0100

    HTM-1468: place a marker on the map at the click location

--- projects/core/src/lib/components/toolbar/clicked-coordinates/clicked-coordinates.component.ts
+++ projects/core/src/lib/components/toolbar/clicked-coordinates/clicked-coordinates.component.ts
@@ -7,6 +7,8 @@ import { isActiveToolbarTool } from '../state/toolbar.selectors';
 import { deregisterTool, registerTool, toggleTool } from '../state/toolbar.actions';
 import { ToolbarComponentEnum } from '../models/toolbar-component.enum';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { FeatureModel } from '@tailormap-viewer/api';
+import { ApplicationStyleService } from '../../../services/application-style.service';
 
 
 @Component({
@@ -21,51 +23,84 @@ export class ClickedCoordinatesComponent implements OnInit, OnDestroy {
   public toolActive$: Observable<boolean>;
   public coordinatesForm = new FormGroup({
     xControl: new FormControl<number | null>(null), yControl: new FormControl<number | null>(null),
-  }, {validators: [validateCoordinates()]});
+  }, { validators: validateCoordinates() });
 
   private destroyed = new Subject();
+  private clickLocationSubject = new Subject<FeatureModel[]>();
+  private clickLocationSubject$ = this.clickLocationSubject.asObservable();
 
   constructor(private store$: Store, private mapService: MapService, private clipboard: Clipboard) {
     this.toolActive$ = this.store$.select(isActiveToolbarTool(ToolbarComponentEnum.SELECT_COORDINATES));
+
     this.toolActive$.pipe(takeUntil(this.destroyed)).subscribe(isActive => {
       if (!isActive) {
-        // TODO remove the marker from the map when closing
         this.coordinatesForm.reset();
+        // TODO maybe not needed
+        this.clickLocationSubject.next([]);
+        // TODO this doesn't seem to work, after deactivating and reactivating we can no longer draw the click location
+        //  remove the layer when the tool is deactivated?
+        // this.mapService.getLayerManager$().pipe(takeUntil(this.destroyed)).subscribe(layerManager => {
+        //   layerManager.removeLayer('tm-clicked-coordinates-layer');
+        // })
       }
     });
+
   }
 
   public ngOnInit(): void {
     this.mapService.createTool$<MapClickToolModel, MapClickToolConfigModel>({
       type: ToolTypeEnum.MapClick,
     })
-      .pipe(takeUntil(this.destroyed), tap(({tool}) => {
-        this.store$.dispatch(registerTool({tool: {id: ToolbarComponentEnum.SELECT_COORDINATES, mapToolId: tool.id}}));
-      }), concatMap(({tool}) => tool.mapClick$), switchMap(mapClick => {
-        // TODO place a marker on the map
+      .pipe(takeUntil(this.destroyed), tap(({ tool }) => {
+        this.store$.dispatch(registerTool({ tool: { id: ToolbarComponentEnum.SELECT_COORDINATES, mapToolId: tool.id } }));
+      }), concatMap(({ tool }) => tool.mapClick$), switchMap(mapClick => {
+        this.pushLocation(mapClick.mapCoordinates);
         return this.mapService.getRoundedCoordinates$(mapClick.mapCoordinates)
           .pipe(map(coordinates => {
             this.coordinatesForm.get('xControl')?.setValue(Number.parseFloat(coordinates[0]));
             this.coordinatesForm.get('yControl')?.setValue(Number.parseFloat(coordinates[1]));
-
-            // TODO cleanup
-            // old code to pass coords string to removed snackbar and clipboard
-            return coordinates.join(', ');
           }));
-      })).subscribe(coords => {
-        // TODO cleanup
-        console.log('clicked, rounded coords', coords);
-    });
+      })).subscribe();
+
+    this.mapService.renderFeatures$('tm-clicked-coordinates-layer', this.clickLocationSubject$, f => {
+      const primaryColor = ApplicationStyleService.getPrimaryColor();
+      // draw a circle with a box inside
+      if (f.__fid === 'clicked-coordinates-point') {
+        return {
+          styleKey: 'tm-clicked-coordinates',
+          zIndex: 2000,
+          pointType: 'circle',
+          pointSize: 15,
+          pointFillColor: 'transparent',
+          pointStrokeColor: primaryColor,
+          pointStrokeWidth: 3,
+        };
+      } else {
+        return {
+          styleKey: 'tm-clicked-coordinates-2',
+          zIndex: 1999,
+          pointType: 'square',
+          pointSize: 5,
+          pointRotation: 45,
+          pointFillColor: 'transparent',
+          pointStrokeColor: primaryColor,
+          pointStrokeWidth: 1,
+        };
+      }
+    }).pipe(takeUntil(this.destroyed)).subscribe();
+
+
   }
 
   public ngOnDestroy() {
+    this.clickLocationSubject.complete();
     this.destroyed.next(null);
     this.destroyed.complete();
-    this.store$.dispatch(deregisterTool({tool: ToolbarComponentEnum.SELECT_COORDINATES}));
+    this.store$.dispatch(deregisterTool({ tool: ToolbarComponentEnum.SELECT_COORDINATES }));
   }
 
   public toggle() {
-    this.store$.dispatch(toggleTool({tool: ToolbarComponentEnum.SELECT_COORDINATES}));
+    this.store$.dispatch(toggleTool({ tool: ToolbarComponentEnum.SELECT_COORDINATES }));
   }
 
   public copy() {
@@ -76,9 +111,20 @@ export class ClickedCoordinatesComponent implements OnInit, OnDestroy {
 
   public goTo() {
     if (this.coordinatesForm.valid) {
-      this.mapService.setCenterAndZoom([// we can safely cast the values to number since the form is valid
-        this.coordinatesForm.get('xControl')?.value as number, this.coordinatesForm.get('yControl')?.value as number], 10);
+      // we can safely cast the values to number since the form is valid
+      const x = this.coordinatesForm.get('xControl')?.value as number;
+      const y = this.coordinatesForm.get('yControl')?.value as number;
+      this.pushLocation([ x, y ]);
+      this.mapService.setCenterAndZoom([ x, y ], 15);
     }
+  }
+
+  private pushLocation(coordinates: number[]) {
+    this.clickLocationSubject.next([{
+      __fid: 'clicked-coordinates-point', geometry: `POINT(${coordinates[0]} ${coordinates[1]})`, attributes: {},
+    }, {
+      __fid: 'clicked-coordinates-point-2', geometry: `POINT(${coordinates[0]} ${coordinates[1]})`, attributes: {},
+    }]);
   }
 }
 
@@ -88,9 +134,9 @@ export function validateCoordinates(): ValidatorFn {
     const yValue: number = form.get('yControl')?.value;
 
     // TODO: get the bounds from the CRS and application max extent and check if the coordinates are within those bounds
-    const isValid = xValue && yValue && xValue > 0 && yValue > 0 ? null : {invalidCoordinates: true};
+    const isValid = xValue && yValue && xValue > 0 && yValue > 0 ? null : { invalidCoordinates: true };
 
-    console.log('form validation, values', xValue, yValue, 'is valid?', isValid);
+    console.log('TODO form validation, values', xValue, yValue, 'is valid?', isValid);
 
     return isValid;
   };
