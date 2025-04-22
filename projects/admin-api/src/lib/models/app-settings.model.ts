d28c323979a3fabce0253a677a29bf76774a787f
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-04-22 14:15:42 +0200

    HTM-1515: Show the application filterGroups json in an editable textarea

--- projects/admin-api/src/lib/models/app-settings.model.ts
+++ projects/admin-api/src/lib/models/app-settings.model.ts
@@ -1,8 +1,9 @@
 import { AppLayerSettingsModel } from './app-layer-settings.model';
-import { I18nSettingsModel, UiSettingsModel } from '@tailormap-viewer/api';
+import { AttributeFilterModel, FilterGroupModel, I18nSettingsModel, UiSettingsModel } from '@tailormap-viewer/api';
 
 export interface AppSettingsModel {
   i18nSettings?: I18nSettingsModel;
   uiSettings?: UiSettingsModel;
   layerSettings: Record<string, AppLayerSettingsModel>;
+  filterGroups?: FilterGroupModel<AttributeFilterModel>[];
 }
