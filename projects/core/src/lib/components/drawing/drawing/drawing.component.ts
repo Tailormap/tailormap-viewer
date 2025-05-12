d8d6f4304f144bd1776917673840b82b6d5c0002
Matthijs Laan <matthijsln@gmail.com>
2025-05-12 17:43:24 +0200

    Add drawing rectangle of fixed width and height

--- projects/core/src/lib/components/drawing/drawing/drawing.component.ts
+++ projects/core/src/lib/components/drawing/drawing/drawing.component.ts
@@ -127,8 +127,17 @@ export class DrawingComponent implements OnInit, OnDestroy {
     if (!this.activeTool) {
       return;
     }
+    const feature = DrawingHelper.getFeature(this.activeTool, $event);
+    if (this.rectangleSize != null && feature.geometry) {
+      const rectangle = this.createRectangleWktAtPoint(feature.geometry, this.rectangleSize.width, this.rectangleSize.height);
+      if (rectangle) {
+        feature.geometry = rectangle;
+      }
+      this.rectangleSize = null;
+      this.draw(DrawingFeatureTypeEnum.RECTANGLE);
+    }
     this.store$.dispatch(addFeature({
-      feature: DrawingHelper.getFeature(this.activeTool, $event),
+      feature,
       selectFeature: true,
     }));
   }
@@ -202,5 +211,20 @@ export class DrawingComponent implements OnInit, OnDestroy {
       ...style,
       label: '',
     });
+  }
+
+  public rectangleSize: { width: number; height: number; label?: string } | null = null;
+
+  public selectRectangleSize(width: number, height: number) {
+    this.rectangleSize = { width, height, label: width + 'x' + height };
+    if (this.activeTool !== DrawingFeatureTypeEnum.POINT) {
+      this.draw(DrawingFeatureTypeEnum.POINT);
+    }
+  }
+
+  private createRectangleWktAtPoint(pointWkt: string, width: number, height: number): string | null {
+    const point = FeatureHelper.fromWKT(pointWkt);
+    const rectangle = FeatureHelper.createRectangleAtPoint(point, width, height);
+    return rectangle ? FeatureHelper.getWKT(rectangle) : null;
   }
 }
