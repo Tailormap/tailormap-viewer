b760d2e0ce4cedb5efbd1e8827cfc5199a5d3a54
stein <steinkobben1@gmail.com>
2025-05-14 15:35:48 +0200

    Use descriptive text in delete filter dialog instead of id

--- projects/admin-core/src/lib/application/application-edit-filters/application-edit-filter/application-edit-filter.component.ts
+++ projects/admin-core/src/lib/application/application-edit-filters/application-edit-filter/application-edit-filter.component.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 import { selectFilterableLayersForApplication, selectFilterGroups, selectSelectedApplicationId } from '../../state/application.selectors';
 import { AttributeFilterModel, FilterGroupModel } from '@tailormap-viewer/api';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ConfirmDialogService } from '@tailormap-viewer/shared';
+import { AttributeFilterHelper, ConfirmDialogService } from '@tailormap-viewer/shared';
 import { AdminSnackbarService } from '../../../shared/services/admin-snackbar.service';
 import {
   deleteApplicationFilterGroup, setApplicationSelectedFilterId, setApplicationSelectedFilterLayerId, updateApplicationFiltersConfig,
@@ -67,8 +67,8 @@ export class ApplicationEditFilterComponent implements OnDestroy {
 
   public delete(attributeFilter: UpdateAttributeFilterModel) {
     this.confirmDelete.confirm$(
-      $localize `:@@admin-core.applications.filters.delete-filter:Delete filter ${attributeFilter.filterId}`,
-      $localize `:@@admin-core.applications.filters.delete-filter-message:Are you sure you want to delete form ${attributeFilter.filterId}? This action cannot be undone.`,
+      $localize `:@@admin-core.applications.filters.delete-filter:Delete filter ${this.getAttributeFilterLabel(attributeFilter)}`,
+      $localize `:@@admin-core.applications.filters.delete-filter-message:Are you sure you want to delete filter ${this.getAttributeFilterLabel(attributeFilter)}? This action cannot be undone.`,
       true,
     )
       .pipe(
@@ -77,7 +77,7 @@ export class ApplicationEditFilterComponent implements OnDestroy {
         tap(() => this.store$.dispatch(deleteApplicationFilterGroup({ filterId: attributeFilter.filterId }))),
       )
       .subscribe(() => {
-        this.adminSnackbarService.showMessage($localize `:@@admin-core.applications.filters.filter-removed:Filter ${attributeFilter.filterId} removed`);
+        this.adminSnackbarService.showMessage($localize `:@@admin-core.applications.filters.filter-removed:Filter ${this.getAttributeFilterLabel(attributeFilter)} removed`);
         this.router.navigateByUrl('/admin/applications/application/' + this.applicationId() + '/filters');
       });
   }
@@ -111,5 +111,15 @@ export class ApplicationEditFilterComponent implements OnDestroy {
 
   public ngOnDestroy(): void {
     this.store$.dispatch(setApplicationSelectedFilterId({ filterId: undefined }));
+  }
+
+  private getAttributeFilterLabel(updateAttributeFilter: UpdateAttributeFilterModel): string {
+    const attributeFilter = updateAttributeFilter.filterGroup.filters.find(filterInGroup =>
+      filterInGroup.id === updateAttributeFilter.filterId);
+    if (!attributeFilter) {
+      return updateAttributeFilter.filterId;
+    }
+    return attributeFilter.attribute + " " + AttributeFilterHelper.getConditionTypes(true).find(conditionType =>
+      conditionType.condition === attributeFilter.condition)?.label;
   }
 }
