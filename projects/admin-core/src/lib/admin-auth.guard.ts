c20fd06eaa1fcc022c62b80429d63a4dc0918e25
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-06-23 16:52:43 +0200

    HTM-1385: Add a guard for the admin paths
    when not logged in the user will be shown the login form

---
+++ projects/admin-core/src/lib/admin-auth.guard.ts
@@ -0,0 +1,24 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthenticatedUserService } from '../../../api/src/lib/services/authenticated-user.service';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminAuthGuard implements CanActivate {
+  constructor(private authService: AuthenticatedUserService, private router: Router) {
+  }
+
+  public canActivate(): Observable<boolean | UrlTree> {
+    return this.authService.getUserDetails$().pipe(
+      map(user =>
+        user
+          ? true
+          : this.router.createUrlTree(['/login'], {
+            queryParams: { routeBeforeLogin: this.router.url },
+          }),
+      ),
+    );
+  }
+}
