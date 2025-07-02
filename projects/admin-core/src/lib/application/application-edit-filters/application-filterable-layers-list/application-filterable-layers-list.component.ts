b7a544d9a0534bd4c3d230d5ab5ab64799e9b1f0
stein <steinkobben1@gmail.com>
2025-07-02 10:52:21 +0200

    List filter for filterable layers

--- projects/admin-core/src/lib/application/application-edit-filters/application-filterable-layers-list/application-filterable-layers-list.component.ts
+++ projects/admin-core/src/lib/application/application-edit-filters/application-filterable-layers-list/application-filterable-layers-list.component.ts
@@ -1,10 +1,13 @@
 import {
-  Component, ChangeDetectionStrategy, Signal, computed, OnDestroy,
+  Component, ChangeDetectionStrategy, Signal, computed, OnDestroy, OnInit, DestroyRef, signal,
 } from '@angular/core';
 import { GeoServiceLayerInApplicationModel } from '../../models/geo-service-layer-in-application.model';
 import { Store } from '@ngrx/store';
 import { selectApplicationSelectedFilterLayerId, selectFilterableLayersForApplication } from '../../state/application.selectors';
 import { setApplicationSelectedFilterLayerId } from '../../state/application.actions';
+import { FormControl } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FilterHelper } from '@tailormap-viewer/shared';
 
 @Component({
   selector: 'tm-admin-filterable-layers-list',
@@ -13,22 +16,34 @@ import { setApplicationSelectedFilterLayerId } from '../../state/application.act
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
-export class ApplicationFilterableLayersListComponent implements OnDestroy {
+export class ApplicationFilterableLayersListComponent implements OnDestroy, OnInit {
 
   public filterableLayers: Signal<GeoServiceLayerInApplicationModel[]> = this.store$.selectSignal(selectFilterableLayersForApplication);
   public selectedLayerId: Signal<string | undefined> = this.store$.selectSignal(selectApplicationSelectedFilterLayerId);
+
+  public layerFilter = new FormControl<string>('');
+  public layerFilterSignal = signal<string>('');
+
   public filterableLayersWithSelected = computed(() => {
     const filterableLayers = this.filterableLayers();
     const selectedLayerId = this.selectedLayerId();
-    return filterableLayers.map(layer => {
+    const filterTerm = this.layerFilterSignal();
+    const layersWithSelected = filterableLayers.map(layer => {
       return {
         ...layer,
         isSelected: layer.appLayerId === selectedLayerId,
       };
     });
+    if (filterTerm) {
+      return FilterHelper.filterByTerm(layersWithSelected, filterTerm, l => l.geoServiceLayer.title);
+    }
+    return layersWithSelected;
   });
 
-  constructor(private store$: Store) { }
+  constructor(
+    private store$: Store,
+    private destroyRef: DestroyRef,
+    ) { }
 
   public setSelectedLayer(layer: GeoServiceLayerInApplicationModel) {
     if (!layer) {
@@ -39,6 +54,16 @@ export class ApplicationFilterableLayersListComponent implements OnDestroy {
 
   public ngOnDestroy(): void {
     this.store$.dispatch(setApplicationSelectedFilterLayerId({ filterLayerId: undefined }));
+  }
+
+  public ngOnInit(): void {
+    this.layerFilter.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(filterTerm => {
+        if (filterTerm !== null) {
+          this.layerFilterSignal.set(filterTerm);
+        }
+      });
   }
 
 }
