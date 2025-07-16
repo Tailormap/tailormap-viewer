fb704d19939aaf7411793ae43fe22e3119cdee25
Geert Plaisier <geert.plaisier@gnamic.nl>
2025-07-16 15:48:24 +0200

    Improved auth guard - show login form and redirect after logging in

--- projects/admin-core/src/lib/admin-auth.guard.ts
+++ projects/admin-core/src/lib/admin-auth.guard.ts
@@ -1,5 +1,5 @@
-import { CanActivate, Router, UrlTree } from '@angular/router';
-import { AuthenticatedUserService } from '../../../api/src/lib/services/authenticated-user.service';
+import { ActivatedRouteSnapshot, CanActivate, RedirectCommand, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthenticatedUserService } from '@tailormap-viewer/api';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
@@ -7,18 +7,23 @@ import { map, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AdminAuthGuard implements CanActivate {
-  constructor(private authService: AuthenticatedUserService, private router: Router) {
-  }
 
-  public canActivate(): Observable<boolean | UrlTree> {
+  constructor(
+    private authService: AuthenticatedUserService,
+    private router: Router,
+  ) {}
+
+  public canActivate(_next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | RedirectCommand> {
     return this.authService.getUserDetails$().pipe(
-      map(user =>
-        user
-          ? true
-          : this.router.createUrlTree(['/login'], {
-            queryParams: { routeBeforeLogin: this.router.url },
-          }),
-      ),
+      map(user => {
+        if (user.isAuthenticated) {
+          return true;
+        }
+        return new RedirectCommand(
+          this.router.parseUrl("/login"),
+          { state: { routeBeforeLogin: state.url } },
+        );
+      }),
     );
   }
 }
