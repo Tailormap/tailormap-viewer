8165e1acf14ecc22409db4e6430a23c8561dbb3b
Matthijs Laan <matthijsln@gmail.com>
2025-07-16 14:23:51 +0200

    Update drawing style icons

--- projects/core/src/lib/components/drawing/drawing-style-icon/drawing-style-icon.component.ts
+++ projects/core/src/lib/components/drawing/drawing-style-icon/drawing-style-icon.component.ts
@@ -30,7 +30,7 @@ export class DrawingStyleIconComponent {
       return '';
     }
     const svgContent = DrawingStyleIconComponent.createSvgContent(this.type, this.featureStyle );
-    const svgImage = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">${svgContent}</svg>`;
+    const svgImage = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">${svgContent}</svg>`;
     return this.domSanitizer.bypassSecurityTrustHtml(svgImage);
   }
 
@@ -40,17 +40,24 @@ export class DrawingStyleIconComponent {
     switch(type) {
       case DrawingFeatureTypeEnum.CIRCLE_SPECIFIED_RADIUS:
       case DrawingFeatureTypeEnum.CIRCLE:
+        return `<circle cx="12" cy="12" r="9" stroke-dasharray="${dashArray}" style="${svgStyle}"></circle>`;
       case DrawingFeatureTypeEnum.ELLIPSE:
-        return `<circle cx="50" cy="50" r="35" stroke-dasharray="${dashArray}" style="${svgStyle}"></circle>`;
+        return `<ellipse cx="12" cy="12" rx="9" ry="4" stroke-dasharray="${dashArray}" style="${svgStyle}"></ellipse>`;
       case DrawingFeatureTypeEnum.POLYGON:
+        return `<path d="M16.9,2.3l4.8,9.8l-7.1,9.5L3.9,17.4L3,7.7C3,7.7,16.9,2.3,16.9,2.3z"  stroke-dasharray="${dashArray}" style="${svgStyle}""/>`;
       case DrawingFeatureTypeEnum.RECTANGLE:
       case DrawingFeatureTypeEnum.RECTANGLE_SPECIFIED_SIZE:
       case DrawingFeatureTypeEnum.SQUARE:
-        return `<rect x="20" y="20" width="60" height="60" rx="5" stroke-dasharray="${dashArray}" style="${svgStyle}"></rect>`;
+        return `<rect x="1" y="1" width="20" height="21" stroke-dasharray="${dashArray}" style="${svgStyle}"></rect>`;
       case DrawingFeatureTypeEnum.LINE:
-       return `<line x1="10" y1="50" x2="90" y2="50" stroke-dasharray="${dashArray}" style="${svgStyle}"></line>`;
+       return `<line x1="1" y1="12" x2="21" y2="12" stroke-dasharray="${dashArray}" style="${svgStyle}"></line>`;
+      case DrawingFeatureTypeEnum.STAR:
+         return `<path d="m14.978 8.427 2.078 3.583 4.142.008-2.064 3.591 2.064 3.591-4.142.007-2.078 3.583-2.077-3.583-4.142-.007
+            2.064-3.591-2.064-3.591 4.142-.008 2.077-3.583Z" transform="scale(1.6) translate(-8,-8)"
+            stroke-dasharray="${dashArray}" style="${DrawingStyleIconComponent.getSvgStyle(style, 1/1.6)}"/></svg>`;
       case DrawingFeatureTypeEnum.LABEL:
-       return `<path d="M38.333 25v10H55v40h10V35h16.667V25H38.333zm-20 26.667h10V75h10V51.667h10v-10h-30v10z" style="${svgStyle}" />`;
+       return `<path d="M38.333 25v10H55v40h10V35h16.667V25H38.333zm-20 26.667h10V75h10V51.667h10v-10h-30v10z"
+            transform="scale(0.24)" style="${DrawingStyleIconComponent.getLabelSvgStyle(style)}" />`;
     }
     return '';
   }
@@ -61,14 +68,22 @@ export class DrawingStyleIconComponent {
     return color ? ColorHelper.getRgbStyleForColor(color) : DrawingStyleIconComponent.defaultRgb;
   }
 
-  private static getSvgStyle(style: DrawingFeatureStyleModel) {
+  private static getSvgStyle(style: DrawingFeatureStyleModel, strokeScale = 1) {
     return [
-      `stroke-opacity: ${style.strokeOpacity}`,
-      `stroke-width: ${style.strokeWidth}`,
+      `stroke-opacity: ${style.strokeOpacity || 100}%`,
+      `stroke-width: ${(style.strokeWidth || 3) / 3 * strokeScale}`,
       `stroke: ${DrawingStyleIconComponent.getRgbStyleForColor(style.strokeColor)}`,
-      `fill-opacity: ${style.fillOpacity}`,
+      `fill-opacity: ${style.fillOpacity || 100}%`,
       `fill: ${DrawingStyleIconComponent.getRgbStyleForColor(style.fillColor)}`,
       `stroke-linecap: round`,
+    ].join(';') + ';';
+  }
+
+  private static getLabelSvgStyle(style: DrawingFeatureStyleModel) {
+    return [
+      `stroke: ${DrawingStyleIconComponent.getRgbStyleForColor(style.labelOutlineColor || 'rgb(255, 255, 255)')}`,
+      `stroke-width: 4`,
+      `fill: ${DrawingStyleIconComponent.getRgbStyleForColor(style.labelColor)}`,
     ].join(';') + ';';
   }
 
