99e0689715c30360845ea9b0a79a71fe0ba6c58e
stein <steinkobben1@gmail.com>
2025-07-18 15:09:18 +0200

    write test for dropdown list filter

--- projects/core/src/lib/components/filter/edit-attribute-filters/dropdown-list-filter/dropdown-list-filter.component.spec.ts
+++ projects/core/src/lib/components/filter/edit-attribute-filters/dropdown-list-filter/dropdown-list-filter.component.spec.ts
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/angular';
 import { DropdownListFilterComponent } from './dropdown-list-filter.component';
+import { SharedImportsModule } from '@tailormap-viewer/shared';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 
 describe('DropdownListFilterComponent', () => {
 
   test('should render', async () => {
-    await render(DropdownListFilterComponent);
-    expect(screen.getByText('dropdown-list-filter works!'));
+    const dropdownListFilterConfiguration = {
+      filterTool: 'DROPDOWN_LIST',
+      attributeValuesSettings: [
+        {
+          value: 'value1',
+          initiallySelected: true,
+          selectable: true,
+          alias: 'Alias1',
+        },
+        {
+          value: 'value2',
+          initiallySelected: false,
+          selectable: true,
+          alias: 'Alias2',
+        },
+      ],
+    };
+    await render(DropdownListFilterComponent, {
+      imports: [ SharedImportsModule, MatIconTestingModule ],
+      inputs: { dropdownListFilterConfiguration },
+    });
+    expect(screen.getByText('Alias1')).toBeInTheDocument();
   });
 
 });
