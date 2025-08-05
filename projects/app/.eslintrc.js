035e6524c28c188efcea453dc5cc705a88c732ac
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-08-05 11:01:19 +0200

    Disable `rxjs/finnish` rule for the app project

--- projects/app/.eslintrc.js
+++ projects/app/.eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
         "tsconfigRootDir": __dirname
       },
       "rules": {
+        "rxjs/finnish": 0,
         "no-restricted-imports": ["error", getInvalidImportsRule("@tailormap-viewer/app")]
       }
     },
