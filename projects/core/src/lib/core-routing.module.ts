8257c3ca45533ba7ddaf846f627f9542df3d5e91
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-10-06 12:43:59 +0200

    HTM-1647: Add a password-reset form / component
    change cursor of password reset link to pointer
    
    apply review comments

--- projects/core/src/lib/core-routing.module.ts
+++ projects/core/src/lib/core-routing.module.ts
@@ -4,9 +4,8 @@ import { LoginComponent, ViewerAppComponent } from './pages';
 import { NavigationErrorRouterService } from './services/navigation-error-router.service';
 
 const routes: Routes = [
-  // IMPORTANT: When you add a route, also add it to the FrontController class of tailormap-api, otherwise a user will get a 404 when
-  // pressing F5 in their browser on your route.
   { path: 'login', component: LoginComponent },
+  { path: 'user', component: LoginComponent },
   { path: 'app/:name', component: ViewerAppComponent },
   { path: 'app', component: ViewerAppComponent },
   { path: 'service/:name', component: ViewerAppComponent },
