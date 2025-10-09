c7359c90069ffb620f635d7210fc39ad33146329
stein <steinkobben1@gmail.com>
2025-10-09 11:41:42 +0200

    move filter button to top

--- projects/core/src/lib/components/filter/filter-list/filter-list.component.ts
+++ projects/core/src/lib/components/filter/filter-list/filter-list.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectFilterGroupsWithLayers } from '../../../filter/state/filter.selectors';
-import { filter, map, Observable, of, switchMap, take } from 'rxjs';
+import { map, Observable, of, switchMap } from 'rxjs';
 import { ExtendedFilterGroupModel } from '../../../filter/models/extended-filter-group.model';
 import { AttributeFilterService } from '../../../services/attribute-filter.service';
-import { resetAttributeFilters } from '../../../filter/state/filter.actions';
-import { ConfirmDialogService } from '@tailormap-viewer/shared';
 
 @Component({
   selector: 'tm-filter-list',
@@ -17,7 +15,6 @@ import { ConfirmDialogService } from '@tailormap-viewer/shared';
 export class FilterListComponent implements OnInit {
   private store$ = inject(Store);
   private attributeFilterService = inject(AttributeFilterService);
-  private confirmService = inject(ConfirmDialogService);
 
   public filters$: Observable<ExtendedFilterGroupModel[]> = of([]);
 
@@ -30,27 +27,4 @@ export class FilterListComponent implements OnInit {
     );
   }
 
-  public resetFilters() {
-    this.filters$.pipe(
-      take(1),
-      map(filterGroups => filterGroups.filter(group => group.source !== "PRESET")),
-    ).subscribe(userAddedGroups => {
-      if (userAddedGroups.length === 0) {
-        this.store$.dispatch(resetAttributeFilters());
-      } else {
-        this.confirmService.confirm$(
-          $localize `:@@core.filter.reset-filters:Reset filters`,
-          $localize `:@@core.filter.reset-filters-confirm:Resetting filters will remove all your added filters. Do you want to proceed?`,
-          true,
-          $localize `:@@core.filter.reset-filters:Reset filters`,
-          $localize `:@@core.common.cancel:Cancel`,
-        ).pipe(
-          filter(confirmed => confirmed),
-        ).subscribe(() => {
-          this.store$.dispatch(resetAttributeFilters());
-        });
-      }
-    });
-
-  }
 }
