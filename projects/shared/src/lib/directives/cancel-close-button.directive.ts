9194843a6a178d052ab0458662b169271f23611b
stein <steinkobben1@gmail.com>
2025-11-11 16:34:27 +0100

    Create directive to use escape key to cancel or close

---
+++ projects/shared/src/lib/directives/cancel-close-button.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, HostListener, Output, EventEmitter, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
+@Directive({
+  selector: '[tmCancelCloseButton]',
+  standalone: false,
+})
+export class CancelCloseButtonDirective {
+  private routerLink = inject(RouterLink, { optional: true });
+  private matDialog = inject(MatDialog);
+  private router = inject(Router);
+
+
+  @Output()
+  public cancelClose = new EventEmitter<void>();
+
+  @HostListener('click')
+  public onClick() {
+    this.triggerAction();
+  }
+
+  @HostListener('window:keydown.escape', ['$event'])
+  public onEscape(event: KeyboardEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.matDialog.openDialogs.length === 0) {
+      this.triggerAction();
+    }
+  }
+
+  private triggerAction() {
+    if (this.routerLink?.urlTree) {
+      this.router.navigateByUrl(this.routerLink.urlTree);
+    }
+    this.cancelClose.emit();
+  }
+}
