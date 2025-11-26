87842c425bef0d4dc7f6025d0372817ad6c88c77
Geert Plaisier <geert.plaisier@gnamic.nl>
2025-11-26 17:23:41 +0100

    Fix lint

--- projects/admin-core/src/lib/admin-auth.guard.ts
+++ projects/admin-core/src/lib/admin-auth.guard.ts
@@ -1,18 +1,14 @@
 import { ActivatedRouteSnapshot, CanActivate, RedirectCommand, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthenticatedUserService } from '@tailormap-viewer/api';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminAuthGuard implements CanActivate {
-
-  constructor(
-    private authService: AuthenticatedUserService,
-    private router: Router,
-  ) {}
-
+  private authService = inject(AuthenticatedUserService);
+  private router = inject(Router);
   public canActivate(_next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | RedirectCommand> {
     return this.authService.getUserDetails$().pipe(
       map(user => {
