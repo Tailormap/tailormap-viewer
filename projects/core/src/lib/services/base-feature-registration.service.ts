3b60c5625be0e14d341d0b4c78a98cc95bae2163
Geert Plaisier <geert.plaisier@gnamic.nl>
2025-12-10 14:41:14 +0100

    Attribute list: Allow registration of components. Fixed some issues.

---
+++ projects/core/src/lib/services/base-feature-registration.service.ts
@@ -0,0 +1,28 @@
+import { signal, Type } from '@angular/core';
+import { AttributeListMenuButtonComponent } from "../components/attribute-list/attribute-list-menu-button/attribute-list-menu-button.component";
+
+export interface AdditionalFeatureModel {
+  component: Type<any>;
+  type: string;
+}
+
+export class BaseFeatureRegistrationService<T extends AdditionalFeatureModel = AdditionalFeatureModel> {
+
+  private registeredComponents = signal<T[]>([]);
+  public registeredAdditionalFeatures = this.registeredComponents.asReadonly();
+
+  public registerAdditionalFeature(component: T) {
+    const registeredComponents = [...this.registeredComponents()];
+    const idx = registeredComponents.findIndex(c => c.type === component.type);
+    if (idx !== -1) {
+      registeredComponents.splice(idx, 1);
+    }
+    registeredComponents.push(component);
+    this.registeredComponents.set([...registeredComponents]);
+  }
+
+  public deregisterAdditionalFeature(componentType: string) {
+    this.registeredComponents.set(this.registeredComponents().filter(c => c.type !== componentType));
+  }
+
+}
