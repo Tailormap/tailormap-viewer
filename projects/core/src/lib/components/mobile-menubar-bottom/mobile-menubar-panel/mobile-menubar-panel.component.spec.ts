03c584914a22d83eb46e2596155a3a5a85b87f35
stein <steinkobben1@gmail.com>
2025-12-12 16:34:45 +0100

    Write test for MobileMenubarPanelComponent

--- projects/core/src/lib/components/mobile-menubar-bottom/mobile-menubar-panel/mobile-menubar-panel.component.spec.ts
+++ projects/core/src/lib/components/mobile-menubar-bottom/mobile-menubar-panel/mobile-menubar-panel.component.spec.ts
@@ -1,11 +1,36 @@
 import { render, screen } from '@testing-library/angular';
 import { MobileMenubarPanelComponent } from './mobile-menubar-panel.component';
+import { SharedModule } from '@tailormap-viewer/shared';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { getMapServiceMock } from '../../../test-helpers/map-service.mock.spec';
+import { of } from 'rxjs';
+import { MenubarService } from '../../menubar';
+
+const setup = async (ac: { componentId: string; dialogTitle: string } | null) => {
+  const mockMenubarService = {
+    getActiveComponent$: () => of(ac),
+    closePanel: () => {},
+  };
+
+  await render(MobileMenubarPanelComponent, {
+    imports: [ SharedModule, MatIconTestingModule ],
+    providers: [
+      getMapServiceMock().provider,
+      { provide: MenubarService, useValue: mockMenubarService },
+    ],
+  });
+};
 
 describe('MobileMenubarPanelComponent', () => {
 
   test('should render', async () => {
-    await render(MobileMenubarPanelComponent);
-    expect(screen.getByText('mobile-menubar-panel works!'));
+    await setup({componentId: 'testpanel', dialogTitle: 'test panel'});
+    expect(screen.queryByText('test panel')).toBeInTheDocument();
+  });
+
+  test('should not render', async () => {
+    await setup(null);
+    expect(screen.queryByText('test panel')).not.toBeInTheDocument();
   });
 
 });
