79e6a9d4a42128a1d27ad1b828360e67b8de2d42
stein <steinkobben1@gmail.com>
2026-01-15 11:43:46 +0100

    Include Home component in mobile menubar module

--- projects/core/src/lib/components/mobile-menubar/mobile-menubar.module.ts
+++ projects/core/src/lib/components/mobile-menubar/mobile-menubar.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { MobileMenubarHomeButtonComponent } from './mobile-menubar-home-button/mobile-menubar-home-button.component';
 import { SharedModule } from '@tailormap-viewer/shared';
 import { RegisteredComponentsModule } from '../registered-components-renderer/registered-components.module';
 import { MatBadge } from '@angular/material/badge';
+import { MobileMenubarBottomComponent } from './mobile-menubar-bottom/mobile-menubar-bottom.component';
+import { CoreSharedModule } from '../../shared/core-shared.module';
+import { MenubarModule } from '../menubar/menubar.module';
+import { MobileMenubarPanelComponent } from './mobile-menubar-panel/mobile-menubar-panel.component';
 import { TocModule } from '../toc';
 import { InfoModule } from '../info';
 import { MobileMenubarHomeComponent } from './mobile-menubar-home/mobile-menubar-home.component';
-import { CoreSharedModule } from '../../shared/core-shared.module';
-import { MenubarModule } from '../menubar/menubar.module';
-
+import { MobileMenubarHomeButtonComponent } from './mobile-menubar-home-button/mobile-menubar-home-button.component';
 
 @NgModule({
   declarations: [
+    MobileMenubarBottomComponent,
+    MobileMenubarPanelComponent,
     MobileMenubarHomeComponent,
     MobileMenubarHomeButtonComponent,
   ],
@@ -23,10 +26,13 @@ import { MenubarModule } from '../menubar/menubar.module';
     MenubarModule,
     TocModule,
     InfoModule,
+    CoreSharedModule,
   ],
   exports: [
+    MobileMenubarBottomComponent,
+    MobileMenubarPanelComponent,
     MobileMenubarHomeComponent,
     MobileMenubarHomeButtonComponent,
   ],
 })
-export class MobileMenubarHomeModule { }
+export class MobileMenubarModule { }
