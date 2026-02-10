efa2ffdb9336bbdba0d0487c45453bb67b5e1402
Mark Prins <1165786+mprins@users.noreply.github.com>
2026-02-10 13:24:51 +0100

    remove some console debugging

--- projects/admin-api/src/lib/models/layer-settings-wms.model.ts
+++ projects/admin-api/src/lib/models/layer-settings-wms.model.ts
@@ -1,6 +1,5 @@
 import { LayerSettingsModel } from './layer-settings.model';
-
-import { WmsStyleModel } from "@tailormap-viewer/api";
+import { WmsStyleModel } from '@tailormap-viewer/api';
 
 export interface LayerSettingsWmsModel extends LayerSettingsModel {
   tilingDisabled?: boolean;
