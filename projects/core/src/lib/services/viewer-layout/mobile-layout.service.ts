6811848f1eadaa2d75574c046a4334a9f9c9f9f4
steinkobben <93925365+steinkobben@users.noreply.github.com>
2026-02-19 10:52:34 +0100

    HTM-1839: Add Coordinate Link Window to mobile menu (#1166)
    * Add CLW to mobile menu
    
    * Use selection-list for layer selection in CLW in mobile panel
    
    * toggle clw map tool when menu button is clicked in mobile layout
    
    * Add comment
    
    * Add translations

--- projects/core/src/lib/services/viewer-layout/mobile-layout.service.ts
+++ projects/core/src/lib/services/viewer-layout/mobile-layout.service.ts
@@ -22,6 +22,7 @@ export class MobileLayoutService {
     BaseComponentTypeEnum.INFO,
     BaseComponentTypeEnum.FILTER,
     BaseComponentTypeEnum.TERRAIN_CONTROLS,
+    BaseComponentTypeEnum.COORDINATE_LINK_WINDOW,
   ];
 
   public isMobileLayoutEnabled$ = this.applicationFeatureSwitchService.isFeatureEnabled$(ApplicationFeature.MOBILE_LAYOUT)
