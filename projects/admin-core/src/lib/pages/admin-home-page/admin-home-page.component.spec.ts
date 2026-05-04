46f36ca34db91f4861750d649d6e1cb0c7176d89
Matthijs Laan <matthijsln@gmail.com>
2026-05-04 19:27:01 +0200

    Fix

--- projects/admin-core/src/lib/pages/admin-home-page/admin-home-page.component.spec.ts
+++ projects/admin-core/src/lib/pages/admin-home-page/admin-home-page.component.spec.ts
@@ -2,12 +2,25 @@ import { render, screen } from '@testing-library/angular';
 import { AdminHomePageComponent } from './admin-home-page.component';
 import { SharedModule } from '@tailormap-viewer/shared';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { OIDCConfigurationService } from '../../oidc/services/oidc-configuration.service';
+import { signal } from '@angular/core';
 
 describe('AdminHomePageComponent', () => {
 
   test('should render', async () => {
+    const mockOIDCConfigurationService = {
+      getOIDCConfigurations$: () => of([]),
+      getExpiringClientSecretConfigurations: () => signal([]),
+    };
+
     await render(AdminHomePageComponent, {
       imports: [ SharedModule, MatIconTestingModule ],
+      componentProviders: [
+        { provide: Store, useValue: {} },
+        { provide: OIDCConfigurationService, useValue: mockOIDCConfigurationService },
+      ],
     });
     expect(screen.getByText('Welcome to Tailormap Admin'));
   });
