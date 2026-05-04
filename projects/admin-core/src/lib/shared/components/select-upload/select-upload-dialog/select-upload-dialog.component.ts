72dab8d79e761601c25661312cccbd96031533ab
stein <steinkobben1@gmail.com>
2026-05-04 18:16:54 +0200

    Do not show description field for legend uploads

--- projects/admin-core/src/lib/shared/components/select-upload/select-upload-dialog/select-upload-dialog.component.ts
+++ projects/admin-core/src/lib/shared/components/select-upload/select-upload-dialog/select-upload-dialog.component.ts
@@ -14,6 +14,7 @@ import { FormControl } from '@angular/forms';
 export interface SelectUploadData {
   uploadId: string | null;
   category: UploadCategoryEnum | string;
+  showDescriptionField: boolean;
 }
 
 export interface SelectUploadResult {
