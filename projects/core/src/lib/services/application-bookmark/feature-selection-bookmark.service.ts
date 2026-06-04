58eed65532bd8b8181893b584e7e10dd1fece270
stein <steinkobben1@gmail.com>
2026-06-04 09:50:02 +0200

    subscribe to rendderFeatures$ to draw the features

--- projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.service.ts
+++ projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.service.ts
@@ -5,13 +5,13 @@ import {
 } from '@tailormap-viewer/api';
 import { MapService } from '@tailormap-viewer/map';
 import { addFilterGroup, removeFilterGroup } from '../../state/filter-state/filter.actions';
-import { selectLayers, selectVisibleLayersWithAttributes, selectVisibleWMSLayersWithoutAttributes } from '../../map';
-import { selectViewerId, selectViewerLoadingState } from '../../state';
+import { selectLayers, selectVisibleLayersWithAttributes } from '../../map';
+import { selectViewerId } from '../../state';
 import { LoadingStateEnum } from '@tailormap-viewer/shared';
 import { catchError, combineLatest, concatMap, filter, first, forkJoin, map, Observable, of, take } from 'rxjs';
 import { CqlFilterHelper, FeaturesFilterHelper } from '../../filter';
 import { featureInfoLoaded, reopenFeatureInfoDialog, setFeatureInfoLayers } from '../../components/feature-info/state/feature-info.actions';
-import { FeatureInfoFeatureModel, FeatureInfoResponseModel } from '../../components';
+import { FeatureInfoResponseModel } from '../../components';
 import { FeatureStylingHelper } from '../../shared';
 
 @Injectable({
@@ -37,19 +37,41 @@ export class FeatureSelectionBookmarkService {
       this.store$.dispatch(addFilterGroup({ filterGroup }));
     }
     if (isEmbedded) {
-      const featureGeometries$: Observable<FeatureModel> = this.getFeatures$(filterGroup)
-        .pipe(
-          concatMap(responses => responses.flatMap(response => response.featuresResponse.features)),
-        );
-      this.mapService.renderFeatures$(
-        'feature-selection-layer',
-        featureGeometries$,
-        FeatureStylingHelper.getDefaultHighlightStyle('attribute-list-highlight-style'),
-        { zoomToFeature: true },
-      )
+      this.getAndHighlightFeatures(filterGroup);
     } else {
       this.getFeaturesAndAddToFeatureInfo(filterGroup);
     }
+  }
+
+  private getFeaturesAndAddToFeatureInfo(filterGroup: FilterGroupModel<AttributeFilterModel>) {
+    this.setFeatureInfoLayers(filterGroup.layerIds);
+    this.getFeatures$(filterGroup)
+      .pipe(take(1))
+      .subscribe((responses) => {
+        const featureInfoResponses = responses
+          .map(response => this.mapToFeatureInfoResponse(response.featuresResponse, response.layerId));
+
+        featureInfoResponses.forEach(response => {
+          this.store$.dispatch(featureInfoLoaded({
+            featureInfo: response,
+          }));
+        });
+        this.mapService.zoomToFeatures(featureInfoResponses.flatMap(r => r.features));
+        this.store$.dispatch(reopenFeatureInfoDialog());
+      });
+  }
+
+  private getAndHighlightFeatures(filterGroup: FilterGroupModel<AttributeFilterModel>) {
+    const featureGeometries$: Observable<FeatureModel> = this.getFeatures$(filterGroup)
+      .pipe(
+        concatMap(responses => responses.flatMap(response => response.featuresResponse.features)),
+      );
+    this.mapService.renderFeatures$(
+      'feature-selection-layer',
+      featureGeometries$,
+      FeatureStylingHelper.getDefaultHighlightStyle('attribute-list-highlight-style'),
+      { zoomToFeature: true },
+    ).subscribe();
   }
 
   private getFeatures$(filterGroup: FilterGroupModel<AttributeFilterModel>): Observable<{ layerId: string, featuresResponse: FeaturesResponseModel }[]> {
@@ -95,24 +117,6 @@ export class FeatureSelectionBookmarkService {
         );
       }),
     )
-  }
-
-  private getFeaturesAndAddToFeatureInfo(filterGroup: FilterGroupModel<AttributeFilterModel>) {
-    this.setFeatureInfoLayers(filterGroup.layerIds);
-    this.getFeatures$(filterGroup)
-      .pipe(take(1))
-      .subscribe((responses) => {
-        const featureInfoResponses = responses
-          .map(response => this.mapToFeatureInfoResponse(response.featuresResponse, response.layerId));
-
-        featureInfoResponses.forEach(response => {
-          this.store$.dispatch(featureInfoLoaded({
-            featureInfo: response,
-          }));
-        });
-        this.mapService.zoomToFeatures(featureInfoResponses.flatMap(r => r.features));
-        this.store$.dispatch(reopenFeatureInfoDialog());
-      });
   }
 
   private mapToFeatureInfoResponse(
