45008d2f40cb29e287c0f29e3e3e93671e58b587
steinkobben <93925365+steinkobben@users.noreply.github.com>
2026-06-19 08:11:25 +0200

    HTM-2018: Check or uncheck all values in checkbox filter (#1370)

--- projects/core/src/lib/components/filter/edit-attribute-filters/checkbox-filter/checkbox-filter.component.ts
+++ projects/core/src/lib/components/filter/edit-attribute-filters/checkbox-filter/checkbox-filter.component.ts
@@ -38,10 +38,31 @@ export class CheckboxFilterComponent {
   @Output()
   public valueChecked = new EventEmitter<{ value: string; checked: boolean; substringFilter: boolean }>();
 
+  @Output()
+  public allValuesChecked = new EventEmitter<{ checked: boolean; regularValues: string[]; substringValues: string[] }>();
+
   constructor() { }
 
   public checkValue(value: AttributeValueSettings, checked: boolean) {
     this.valueChecked.emit({ value: value.value, checked, substringFilter: value.useAsIlikeSubstringFilter ?? false });
+  }
+
+  public someValuesSelected(): boolean {
+    return this.attributeValuesSettings.some(v => v.selectable && v.initiallySelected);
+  }
+
+  public toggleAll() {
+    const checked = !this.someValuesSelected();
+    const selectableValues = this.attributeValuesSettings.filter(v => v.selectable);
+    const regularValues = selectableValues.filter(v => !v.useAsIlikeSubstringFilter).map(v => v.value);
+    const substringValues = selectableValues.filter(v => v.useAsIlikeSubstringFilter).map(v => v.value);
+    this.allValuesChecked.emit({ checked, regularValues, substringValues });
+  }
+
+  public getToggleAllTooltip(): string {
+    return this.someValuesSelected()
+      ? $localize `:@@core.filter.checkbox.uncheck-all:Uncheck all values`
+      : $localize `:@@core.filter.checkbox.check-all:Check all values`;
   }
 
 }
