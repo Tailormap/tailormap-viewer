c2185758db343538389532da22506968da073415
Matthijs Laan <matthijsln@gmail.com>
2026-07-10 17:01:11 +0200

    HTM-2096: Add option to hide empty values in feature info

--- projects/api/src/lib/models/component-config/feature-info-config.model.ts
+++ projects/api/src/lib/models/component-config/feature-info-config.model.ts
@@ -3,4 +3,5 @@ import { ComponentBaseConfigModel } from '../component-base-config.model';
 export interface FeatureInfoConfigModel extends ComponentBaseConfigModel {
   defaultShowDropdown: boolean;
   showEditButton?: boolean;
+  hideEmptyValues?: boolean;
 }
