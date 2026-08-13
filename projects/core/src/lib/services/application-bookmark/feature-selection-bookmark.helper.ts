db06895cb21630d91df4c0be9be951a54523a376
Matthijs Laan <matthijsln@gmail.com>
2026-08-13 18:24:51 +0200

    Use CrossOriginApiService to handle feature selection messages and use takeUntilDestroyed()

--- projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.helper.ts
+++ projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.helper.ts
@@ -5,11 +5,6 @@ import { FeatureSelectionBookmarkFragment } from './application-bookmark-fragmen
 import { v4 as uuidv4 } from 'uuid';
 import { FeatureInfoResponseModel } from "../../components/feature-info/models/feature-info-response.model";
 
-export interface FeatureSelectionMessage {
-  type: 'tailormap-feature-selection';
-  value: string;
-}
-
 export class FeatureSelectionBookmarkHelper {
   private static PART_SEPARATOR = ';';
   private static SERVICE_LAYER_SEPARATOR = '/';
@@ -116,15 +111,6 @@ export class FeatureSelectionBookmarkHelper {
       template: response.template || null,
       layerId,
     };
-  }
-
-  public static isFeatureSelectionMessage(data: unknown): data is FeatureSelectionMessage {
-    return !!data
-      && typeof data === 'object'
-      && 'type' in data
-      && data.type === 'tailormap-feature-selection'
-      && 'value' in data
-      && typeof data.value === 'string';
   }
 
   public static createFidSelectionFragment(serviceId: string, layerName: string, fid: string): string {
