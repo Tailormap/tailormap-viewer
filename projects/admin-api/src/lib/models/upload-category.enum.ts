39ba83d24a26f3d5fb17937c4685b6b826ed752d
Mark Prins <1165786+mprins@users.noreply.github.com>
2026-08-28 12:47:16 +0200

    HTM-2221: Add upload categories `unrestricted` and `layer-attached-file`

--- projects/admin-api/src/lib/models/upload-category.enum.ts
+++ projects/admin-api/src/lib/models/upload-category.enum.ts
@@ -3,4 +3,6 @@ export enum UploadCategoryEnum {
   LEGEND = 'legend',
   IMAGE = 'image',
   SSO_IMAGE = 'sso-image',
+  LAYER_ATTACHED_FILE = 'layer-attached-file',
+  UNRESTRICTED = 'unrestricted',
 }
