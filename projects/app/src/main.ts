f6beec37706fd4fedb256c0d214442934efc98c7
Geert Plaisier <geert.plaisier@gnamic.nl>
2026-09-02 16:21:34 +0200

    Migrate to standalone - phase 3

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,12 +1,29 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { provideHttpClient, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
+import { TailormapApiConstants } from '@tailormap-viewer/api';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { CoreModule } from '@tailormap-viewer/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
 
 const main = async () => {
   try {
-    await platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], });
+    await bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, CoreModule.forRoot({
+            production: environment.production,
+            viewerBaseUrl: environment.viewerBaseUrl,
+        }), BrowserAnimationsModule, ...environment.imports),
+        provideHttpClient(withInterceptorsFromDi(), withXsrfConfiguration({
+            cookieName: TailormapApiConstants.XSRF_COOKIE_NAME,
+            headerName: TailormapApiConstants.XSRF_HEADER_NAME,
+        }))
+    ]
+});
   } catch (error) {
     console.error(error);
   }
