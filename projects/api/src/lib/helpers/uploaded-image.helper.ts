6d529247f4b0b56776de5d944415f61b7d3a51b2
Mark Prins <1165786+mprins@users.noreply.github.com>
2026-09-03 17:22:17 +0200

    HTM-2223: Uploaded legend images must be attached to a layer

--- projects/api/src/lib/helpers/uploaded-image.helper.ts
+++ projects/api/src/lib/helpers/uploaded-image.helper.ts
@@ -2,4 +2,9 @@ export class UploadedImageHelper {
   public static getUrlForFile(id: string, category: string, fileName: string = 't') {
     return `/api/uploads/${category}/${id}/${fileName}`;
   }
+
+  public static getAdminUrlForLegend(id: string, category: string, fileName: string = 't') {
+    return `/api/admin/uploads/${category}/${id}/${fileName}`;
+  }
+
 }
