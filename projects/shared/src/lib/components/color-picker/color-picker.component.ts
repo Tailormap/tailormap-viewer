2f16ab61d9eb9d162aaf5a0cd961772285c81d0c
stein <steinkobben1@gmail.com>
2026-09-07 18:23:10 +0200

    add aria-labels to default color options

--- projects/shared/src/lib/components/color-picker/color-picker.component.ts
+++ projects/shared/src/lib/components/color-picker/color-picker.component.ts
@@ -8,55 +8,58 @@ import { OverlayRef } from '../../services/overlay/overlay-ref';
 import { BrowserHelper } from '../../helpers';
 import { PopoverPositionEnum } from '../../services';
 
-const defaultColors: Array<string | undefined> = [
-  'rgb(136, 14, 79)',
-  'rgb(165, 39, 20)',
-  'rgb(170, 0, 0)',
-  'rgb(230, 81, 0)',
-  'rgb(249, 168, 37)',
-  'rgb(255, 214, 0)',
-  'rgb(129, 119, 23)',
-  'rgb(85, 139, 47)',
-  'rgb(9, 113, 56)',
-  'rgb(0, 96, 100)',
-  'rgb(1, 87, 155)',
-  'rgb(26, 35, 126)',
-  'rgb(103, 58, 183)',
-  'rgb(78, 52, 46)',
-  'rgb(194, 24, 91)',
-  'rgb(255, 82, 82)',
-  'rgb(204, 0, 0)',
-  'rgb(245, 124, 0)',
-  'rgb(251, 192, 45)',
-  'rgb(255, 234, 0)',
-  'rgb(175, 180, 43)',
-  'rgb(124, 179, 66)',
-  'rgb(15, 157, 88)',
-  'rgb(0, 151, 167)',
-  'rgb(2, 136, 209)',
-  'rgb(57, 73, 171)',
-  'rgb(156, 39, 176)',
-  'rgb(121, 85, 72)',
-  'rgb(244, 143, 177)',
-  'rgb(237, 162, 155)',
-  'rgb(234, 153, 153)',
-  'rgb(255, 204, 128)',
-  'rgb(250, 218, 128)',
-  'rgb(255, 255, 141)',
-  'rgb(230, 238, 156)',
-  'rgb(197, 225, 165)',
-  'rgb(135, 206, 172)',
-  'rgb(178, 235, 242)',
-  'rgb(161, 194, 250)',
-  'rgb(159, 168, 218)',
-  'rgb(206, 147, 216)',
-  'rgb(188, 170, 164)',
-  'rgb(255, 255, 255)',
-  'rgb(189, 189, 189)',
-  'rgb(117, 117, 117)',
-  'rgb(66, 66, 66)',
-  'rgb(0, 0, 0)',
+const colorDefinitions: Array<[string, string]> = [
+  [ 'rgb(136, 14, 79)', $localize`:@@shared.color-picker.color.dark-pink:Dark pink` ],
+  [ 'rgb(165, 39, 20)', $localize`:@@shared.color-picker.color.dark-red-brown:Dark red brown` ],
+  [ 'rgb(170, 0, 0)', $localize`:@@shared.color-picker.color.dark-red:Dark red` ],
+  [ 'rgb(230, 81, 0)', $localize`:@@shared.color-picker.color.dark-orange:Dark orange` ],
+  [ 'rgb(249, 168, 37)', $localize`:@@shared.color-picker.color.amber:Amber` ],
+  [ 'rgb(255, 214, 0)', $localize`:@@shared.color-picker.color.yellow:Yellow` ],
+  [ 'rgb(129, 119, 23)', $localize`:@@shared.color-picker.color.olive:Olive` ],
+  [ 'rgb(85, 139, 47)', $localize`:@@shared.color-picker.color.green:Green` ],
+  [ 'rgb(9, 113, 56)', $localize`:@@shared.color-picker.color.dark-green:Dark green` ],
+  [ 'rgb(0, 96, 100)', $localize`:@@shared.color-picker.color.teal:Teal` ],
+  [ 'rgb(1, 87, 155)', $localize`:@@shared.color-picker.color.dark-blue:Dark blue` ],
+  [ 'rgb(26, 35, 126)', $localize`:@@shared.color-picker.color.navy:Navy` ],
+  [ 'rgb(103, 58, 183)', $localize`:@@shared.color-picker.color.purple:Purple` ],
+  [ 'rgb(78, 52, 46)', $localize`:@@shared.color-picker.color.dark-brown:Dark brown` ],
+  [ 'rgb(194, 24, 91)', $localize`:@@shared.color-picker.color.pink:Pink` ],
+  [ 'rgb(255, 82, 82)', $localize`:@@shared.color-picker.color.coral-red:Coral red` ],
+  [ 'rgb(204, 0, 0)', $localize`:@@shared.color-picker.color.red:Red` ],
+  [ 'rgb(245, 124, 0)', $localize`:@@shared.color-picker.color.orange:Orange` ],
+  [ 'rgb(251, 192, 45)', $localize`:@@shared.color-picker.color.golden-yellow:Golden yellow` ],
+  [ 'rgb(255, 234, 0)', $localize`:@@shared.color-picker.color.bright-yellow:Bright yellow` ],
+  [ 'rgb(175, 180, 43)', $localize`:@@shared.color-picker.color.yellow-green:Yellow green` ],
+  [ 'rgb(124, 179, 66)', $localize`:@@shared.color-picker.color.light-green:Light green` ],
+  [ 'rgb(15, 157, 88)', $localize`:@@shared.color-picker.color.medium-green:Medium green` ],
+  [ 'rgb(0, 151, 167)', $localize`:@@shared.color-picker.color.cyan:Cyan` ],
+  [ 'rgb(2, 136, 209)', $localize`:@@shared.color-picker.color.blue:Blue` ],
+  [ 'rgb(57, 73, 171)', $localize`:@@shared.color-picker.color.indigo:Indigo` ],
+  [ 'rgb(156, 39, 176)', $localize`:@@shared.color-picker.color.violet:Violet` ],
+  [ 'rgb(121, 85, 72)', $localize`:@@shared.color-picker.color.brown:Brown` ],
+  [ 'rgb(244, 143, 177)', $localize`:@@shared.color-picker.color.light-pink:Light pink` ],
+  [ 'rgb(237, 162, 155)', $localize`:@@shared.color-picker.color.salmon:Salmon` ],
+  [ 'rgb(234, 153, 153)', $localize`:@@shared.color-picker.color.light-red:Light red` ],
+  [ 'rgb(255, 204, 128)', $localize`:@@shared.color-picker.color.light-orange:Light orange` ],
+  [ 'rgb(250, 218, 128)', $localize`:@@shared.color-picker.color.light-amber:Light amber` ],
+  [ 'rgb(255, 255, 141)', $localize`:@@shared.color-picker.color.light-yellow:Light yellow` ],
+  [ 'rgb(230, 238, 156)', $localize`:@@shared.color-picker.color.yellow-lime:Yellow lime` ],
+  [ 'rgb(197, 225, 165)', $localize`:@@shared.color-picker.color.pale-green:Pale green` ],
+  [ 'rgb(135, 206, 172)', $localize`:@@shared.color-picker.color.mint:Mint` ],
+  [ 'rgb(178, 235, 242)', $localize`:@@shared.color-picker.color.light-cyan:Light cyan` ],
+  [ 'rgb(161, 194, 250)', $localize`:@@shared.color-picker.color.light-blue:Light blue` ],
+  [ 'rgb(159, 168, 218)', $localize`:@@shared.color-picker.color.periwinkle:Periwinkle` ],
+  [ 'rgb(206, 147, 216)', $localize`:@@shared.color-picker.color.lavender:Lavender` ],
+  [ 'rgb(188, 170, 164)', $localize`:@@shared.color-picker.color.rosy-brown:Rosy brown` ],
+  [ 'rgb(255, 255, 255)', $localize`:@@shared.color-picker.color.white:White` ],
+  [ 'rgb(189, 189, 189)', $localize`:@@shared.color-picker.color.light-grey:Light grey` ],
+  [ 'rgb(117, 117, 117)', $localize`:@@shared.color-picker.color.grey:Grey` ],
+  [ 'rgb(66, 66, 66)', $localize`:@@shared.color-picker.color.dark-grey:Dark grey` ],
+  [ 'rgb(0, 0, 0)', $localize`:@@shared.color-picker.color.black:Black` ],
 ];
+
+const defaultColors: Array<string | undefined> = colorDefinitions.map(([color]) => color);
+const colorLabels = new Map<string, string>(colorDefinitions);
 
 @Component({
   selector: 'tm-color-picker',
@@ -227,5 +230,13 @@ export class ColorPickerComponent implements OnInit, OnDestroy {
       return;
     }
     navigator.clipboard.writeText(this.color!).then(() => {});
+  }
+
+  public getColorLabel(color: string | undefined): string {
+    if (!color) {
+      return $localize`:@@shared.color-picker.no-color:No color`;
+    }
+    const colorLabel = colorLabels.get(color) ?? color;
+    return $localize`:@@shared.color-picker.pick:Pick ${colorLabel}`;
   }
 }
