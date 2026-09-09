1d2ae7df3ba88032d1795e51bc49035aba5720dc
stein <steinkobben1@gmail.com>
2026-09-09 11:03:44 +0200

    Use defaultIfEmpty instead of startWith for bookmark to make it not always use an empty string

--- projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.service.ts
+++ projects/core/src/lib/services/application-bookmark/feature-selection-bookmark.service.ts
@@ -8,7 +8,9 @@ import { addFilterGroup, removeFilterGroup } from '../../state/filter-state/filt
 import { selectVisibleAppLayerIds, selectLayer, selectLayers, selectVisibleLayersWithAttributes } from '../../map';
 import { selectViewerId } from '../../state';
 import { LoadingStateEnum, SnackBarMessageComponent, SnackBarMessageOptionsModel } from '@tailormap-viewer/shared';
-import { BehaviorSubject, catchError, combineLatest, concatMap, filter, forkJoin, map, Observable, of, startWith, take } from 'rxjs';
+import {
+  BehaviorSubject, catchError, combineLatest, concatMap, defaultIfEmpty, filter, forkJoin, map, Observable, of, take,
+} from 'rxjs';
 import { CqlFilterHelper, FeaturesFilterHelper, FeaturesFilters } from '../../filter';
 import {
   emptyFeatureInfo,
@@ -286,10 +288,11 @@ export class FeatureSelectionBookmarkService {
   public getFidSelectionUrl$(appLayerId: string, fid: string): Observable<string | null> {
     return combineLatest([
       this.store$.select(selectLayer(appLayerId)),
-      this.bookmarkService.getBookmarkValue$().pipe(startWith('')),
+      this.bookmarkService.getBookmarkValue$().pipe(defaultIfEmpty('')),
     ]).pipe(
       take(1),
       map(([ layer, bookmark ]) => {
+        console.log('bookmark', bookmark);
         if (!layer || !fid) {
           return null;
         }
